728e5c29b5346dcfffbc4a759f2b2b4751a257b5
James Sangalli <j.l.sangalli@gmail.com>
2018-02-10 10:18:35 +0800

    with sample test code for signing but having difficulties with targets

--- Trust/Market/SignOrders.swift
+++ Trust/Market/SignOrders.swift
@@ -2,44 +2,84 @@
 
 import Foundation
 
+
 public class SignOrders
 {
+    //TODO get current account and assign it for signing
+    //private let account : Account = Account.init()
+    public static let CONTRACT_ADDR : String = "0xd9864b424447B758CdE90f8655Ff7cA4673956bf"
+
     //takes a list of orders and returns a list of signature objects
-    func signOrder(orders : List<Order>, account: Account) -> List<Result<Data, KeystoreError>>
+    func signOrder(orders : Array<Order>, account: Account) -> List<Result<Data, KeystoreError>>
     {
-        var signatureObjects : List<Result<Data, KeystoreError>>
+        var signatureObjects : Array<Result<Data, KeystoreError>>
         //EtherKeystore.signMessage(encodeMessage(), )
         for i in orders.length
         {
             //sign each order
-            //signatureObjects.add(EtherKeystore.signMessage(encodeMessage(), ))
+            //TODO check casting to string
+            var message = encodeMessageForTrade(price: orders.get(i).price,
+                    expiryTimestamp: orders.get(i).expiryTimeStamp, tickets: orders.get(i).ticketIndices) as String
+            var signedOrder = EtherKeystore.signMessage(message, self.account, false)
+            signatureObjects.add(signedOrder)
         }
         return signatureObjects
     }
     
     
-//    public static byte[] encodeMessageForTrade(BigInteger price, BigInteger expiryTimestamp, short[] tickets)
-//    {
-//        byte[] priceInWei = price.toByteArray();
-//        byte[] expiry = expiryTimestamp.toByteArray();
-//        ByteBuffer message = ByteBuffer.allocate(84 + tickets.length * 2);
-//        byte[] leadingZeros = new byte[32 - priceInWei.length];
-//        message.put(leadingZeros);
-//        message.put(priceInWei);
-//        byte[] leadingZerosExpiry = new byte[32 - expiry.length];
-//        message.put(leadingZerosExpiry);
-//        message.put(expiry);
-//        byte[] contract = hexStringToBytes(CONTRACT_ADDR.substring(2));
-//        System.out.println("length of contract: " + contract.length);
-//        message.put(contract);
-//        ShortBuffer shortBuffer = message.slice().asShortBuffer();
-//        shortBuffer.put(tickets);
-//
-//        return message.array();
-//    }
-//
-    func encodeMessage(price : BigInt, expiryTimestamp : BigInt, tickets : short[])) -> String
+    func encodeMessageForTrade(price : BigInt, expiryTimestamp : BigInt, tickets : [ushort]) -> [byte]
     {
-        return ""
+        var priceInWei : [byte] = price.toByteArray();
+        var expiry : [byte] = expiryTimestamp.toByteArray();
+        var message : [byte] = ByteBuffer.allocate(84 + tickets.length * 2);
+        var leadingZeros : [byte] = [32 - priceInWei.length];
+        message.put(leadingZeros);
+        message.put(priceInWei);
+        var leadingZerosExpiry : [byte] = [32 - expiry.length];
+        message.put(leadingZerosExpiry);
+        message.put(expiry);
+        var contract : [byte] = hexStringToBytes(CONTRACT_ADDR.substring(2));
+        System.out.println("length of contract: " + contract.length);
+        message.put(contract);
+        var shortBuffer = message.slice().asShortBuffer();
+        shortBuffer.put(tickets);
+
+        return message.array();
     }
+
+    func hexStringToByteArray(hexString : String) -> [byte]
+    {
+        var cleanInput : String = cleanHexPrefix(input);
+
+        var len = cleanInput.length();
+
+        if len == 0 {
+            return byte[]{};
+        }
+
+        var data;
+        var startIdx;
+        if len % 2 != 0
+        {
+            data = byte[(len / 2) + 1];
+            data[0] = Character.digit(cleanInput.charAt(0), 16);
+            startIdx = 1;
+        }
+        else
+        {
+            data = byte[len / 2];
+            startIdx = 0;
+        }
+
+        var i = startIdx
+        while i < len
+        {
+            data[(i + 1) / 2] = (byte) ((Character.digit(cleanInput.charAt(i), 16) << 4)
+            + Character.digit(cleanInput.charAt(i + 1), 16))
+            i += 2
+        }
+
+        return data;
+    }
+
 }
--- TrustTests/Foundation/EtherNumberFormatterTests.swift
+++ TrustTests/Foundation/EtherNumberFormatterTests.swift
@@ -5,6 +5,36 @@ import BigInt
 import XCTest
 
 class EtherNumberFormatterTests: XCTestCase {
+
+    //TODO remove
+    let account : Account = self.createAccount("test")
+    var testOrdersList : Array<Order>
+
+    func testInit()
+    {
+        //set up test orders
+        let testOrder1 = Order()
+        testOrder1.price = BigInt.min
+        testOrder1.expiryTimeStamp = BigInt.min
+        testOrder1.contractAddress = SignOrders.CONTRACT_ADDR
+        //in test we will create signatures
+        testOrder1.v = 27
+        testOrder1.hexR = "0x9CAF1C785074F5948310CD1AA44CE2EFDA0AB19C308307610D7BA2C74604AE98"
+        testOrder1.hexS = "0x23D8D97AB44A2389043ECB3C1FB29C40EC702282DB6EE1D2B2204F8954E4B451"
+        testOrdersList.append(testOrder1)
+
+        testSigningOrders()
+    }
+
+    func testSigningOrders()
+    {
+        var signOrders = SignOrders()
+        //TODO once working, do this 2016 times as a test
+        signOrders.signOrder(testOrders, account)
+    }
+
+    //TODO remove to here
+
     let fullFormatter = EtherNumberFormatter(locale: Locale(identifier: "en_US_POSIX"))
     let shortFormatter: EtherNumberFormatter = {
         var formatter = EtherNumberFormatter(locale: Locale(identifier: "en_US_POSIX"))
