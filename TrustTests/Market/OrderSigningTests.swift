d82ef876dff63c09a53d527cfe47336063b8ab21
James Sangalli <j.l.sangalli@gmail.com>
2018-02-11 16:05:00 +0800

    signing class in order, needs signature to be tested with contract

--- TrustTests/Market/OrderSigningTests.swift
+++ TrustTests/Market/OrderSigningTests.swift
@@ -4,32 +4,26 @@ import TrustKeystore
 import RealmSwift
 import BigInt
 
-//TODO remove and replace with main struct
-struct Order {
-    var price: BigInt?;
-    var ticketIndices: [Int]?;
-    var expiryTimeStamp: BigInt?;
-    var contractAddress: String?;
-}
-
-class OrderSigningTests : XCTestCase
-{
+class OrderSigningTests : XCTestCase  {
     //let account = self.createAccount("test")
-    var testOrdersList : [Order] = []
+    var testOrdersList : Array<Order> = Array<Order>()
+    var keyStore = FakeKeystore()
 
-    func testInit()
-    {
+    func testInit() {
         //set up test orders
-        let indices = [1, 2]
-        let testOrder1 = Order(price : 1, ticketIndices : indices, expiryTimeStamp : 0, contractAddress : "0xd9864b424447B758CdE90f8655Ff7cA4673956bf")
+        var indices = [UInt16]()
+        indices.append(1)
+        indices.append(2)
+        let testOrder1 = Order(price : BigInt(0), ticketIndices: indices, expiryTimeStamp: BigInt(0), contractAddress : "0xd9864b424447B758CdE90f8655Ff7cA4673956bf")
         testOrdersList.append(testOrder1)
         testSigningOrders()
     }
 
-    func testSigningOrders()
-    {
-        var signOrders = SignOrders()
+    func testSigningOrders() {
+        let signOrders = SignOrders()
         //TODO once working, do this 2016 times as a test
-        //signOrders.signOrder(testOrdersList, account)
+        let account = keyStore.createAccount(password: "deleteOnceWorking")
+        var signedOrders : Array<SignedOrder> = signOrders.signOrders(orders: testOrdersList, account: account)
+        print(signedOrders.popLast())
     }
 }
