965317d720191d19b885150c556c3530cab5ad2a
James Sangalli <j.l.sangalli@gmail.com>
2018-02-13 16:27:16 +0800

    tidied up tests but encoding message is incorrect

--- TrustTests/Market/OrderSigningTests.swift
+++ TrustTests/Market/OrderSigningTests.swift
@@ -5,28 +5,29 @@ import RealmSwift
 import BigInt
 
 class OrderSigningTests : XCTestCase  {
-    //let account = self.createAccount("test")
-    var testOrdersList : Array<Order> = Array<Order>()
-    var keyStore = FakeEtherKeystore()
 
-    func testInit() {
+    //TODO fix signing errors
+    func testSigningOrders() {
+
+        var testOrdersList : Array<Order> = Array<Order>()
+        var keyStore = FakeEtherKeystore()
+
         //set up test orders
-        var indices = [UInt16]()
+        var indices = [UInt8]()
         indices.append(1)
         indices.append(2)
-        let testOrder1 = Order(price : BigInt(0), ticketIndices: indices, expiryTimeStamp: BigInt(0), contractAddress : "0x0dcd2f752394c41875e259e00bb44fd505297caf")
+        let testOrder1 = Order(price : BigInt(0), ticketIndices: indices,
+                expiryTimeStamp: BigInt(0), contractAddress : "fFAB5Ce7C012bc942F5CA0cd42c3C2e1AE5F0005")
         testOrdersList.append(testOrder1)
-        testSigningOrders()
-    }
 
-    func testSigningOrders() {
         let signOrders = SignOrders()
         var account = keyStore.createAccount(password: "deleteOnceWorking")
         print(account)
+
         var signedOrders : Array<SignedOrder> = signOrders.signOrders(orders: testOrdersList, account: account)
 
         var signature = try! keyStore.signMessage(((signedOrders.first?.message)!) as! String, for: account).dematerialize().hexString
-        print("v: " + signature.substring(from: 128))
+        print("v: " + Int(signature.substring(from: 128), radix: 16)!.description)
         print("r: 0x" + signature.substring(to: 64))
         print("s: 0x" + signature.substring(from: 64))
 
@@ -39,4 +40,4 @@ class OrderSigningTests : XCTestCase  {
     }
 }
 
-//0, [1,2], 28, "0xee42d612316d2c57b9b036e4e2e08e37546f43fccf45d816de82def9f3b9d54b", "0x17b4764146b5ffcc1faa85baa185288b9e03743a3ab4ada0af03e3a86772dd3f1c"
+//0,[1,2], 27, "0x97d4ccdf86b03af7bf81f2d61e1e98f8f86c55c1eaa737e562c9471ea868c03a", "0x5b9f9976d4507245cdc090538cbb97122fe3c71d84bf3f42ad03feed267ee72f1b"
