0a40469ec63a12d0cb73d7f1de20df5cd35094db
James Sangalli <j.l.sangalli@gmail.com>
2018-02-19 19:09:13 +0800

    use sample public key

--- TrustTests/Market/OrderRequestTest.swift
+++ TrustTests/Market/OrderRequestTest.swift
@@ -38,7 +38,8 @@ class OrderRequestTest : XCTestCase  {
 
         var privateKey = keyStore.exportPrivateKey(account: account)
 
-        var publicKey = try! Secp256k1.shared.getPublicKeyFromPrivateKey(from: privateKey.dematerialize()).hexString
+        var publicKey = "qTIttEQTN2OhfJJimQInXPYwz9EohLtg2MFMrnCtTmSpMi20RBM3Y6F8kmKZAidc9jDP0SiEu2DYwUyucK1OZKk",
+            //try! Secp256k1.shared.getPublicKeyFromPrivateKey(from: privateKey.dematerialize()).hexString
 
         //TODO get public key or change server to take address
         OrdersRequest.init().giveOrderToServer(signedOrders: signedOrders, publicKeyHex: publicKey) {
