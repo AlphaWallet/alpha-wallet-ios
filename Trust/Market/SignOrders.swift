c4550d017cdf73acd0110e758c2d5791e9bf29d8
James Sangalli <j.l.sangalli@gmail.com>
2018-02-22 22:26:37 +0800

    added proper base methods to handle queue server, changed http lib

--- Trust/Market/SignOrders.swift
+++ Trust/Market/SignOrders.swift
@@ -1,30 +1,32 @@
 import BigInt
 import TrustKeystore
+import Trust
 
 public struct Order {
-    var price: [UInt8]
-    var ticketIndices: [UInt16]
-    var expiryBuffer: [UInt8]
+    var price: BigInt
+    var start: UInt16
+    var count: Int
+    var expiryBuffer: BigInt
     var contractAddress: String
 }
 
 public struct SignedOrder {
-    var order : Order
-    var message : Data
-    var signature : String
-}
-
-extension String {
-    var hexa2Bytes: [UInt8] {
-        let hexa = Array(characters)
-        return stride(from: 0, to: count, by: 2).flatMap { UInt8(String(hexa[$0..<$0.advanced(by: 2)]), radix: 16) }
-    }
+    var order: Order
+    var message: Data
+    var signature: String
 }
 
 extension BinaryInteger {
     var data: Data {
         var source = self
         return Data(bytes: &source, count: MemoryLayout<Self>.size)
+    }
+}
+
+extension String {
+    var hexa2Bytes: [UInt8] {
+        let hexa = Array(characters)
+        return stride(from: 0, to: count, by: 2).flatMap { UInt8(String(hexa[$0..<$0.advanced(by: 2)]), radix: 16) }
     }
 }
 
@@ -42,9 +44,14 @@ public class SignOrders {
         var signedOrders : Array<SignedOrder> = Array<SignedOrder>()
 
         for i in 0...orders.count - 1 {
-            let message : [UInt8] = encodeMessageForTrade(price: orders[i].price,
-                    expiryBuffer: orders[i].expiryBuffer, tickets: orders[i].ticketIndices,
-                    contractAddress : orders[i].contractAddress)
+            let message : [UInt8] =
+            encodeMessageForTrade(
+                    price: orders[i].price,
+                    expiryBuffer: orders[i].expiryBuffer,
+                    start: orders[i].start,
+                    count: orders[i].count,
+                    contractAddress : orders[i].contractAddress
+            )
             let messageData = Data(bytes: message)
 
             let signature = try! keyStore.signMessageData(messageData, for: account)
@@ -55,18 +62,17 @@ public class SignOrders {
         return signedOrders
     }
 
-    func encodeMessageForTrade(price : [UInt8], expiryBuffer : [UInt8],
-                               tickets : [UInt16], contractAddress : String) -> [UInt8]
-    {
+    func encodeMessageForTrade(price: BigInt, expiryBuffer: BigInt, start: UInt16,
+                               count: Int, contractAddress: String) -> [UInt8] {
         //ticket count * 2 because it is 16 bits not 8
-        let arrayLength: Int = 84 + tickets.count * 2
+        let arrayLength: Int = 84 + count * 2
         var buffer = [UInt8]()
         buffer.reserveCapacity(arrayLength)
 
-        var priceInWei : [UInt8] = price
-        var expiry : [UInt8] = expiryBuffer
+        var priceInWei: [UInt8] = toByteArray(price)
+        var expiry: [UInt8] = toByteArray(expiryBuffer)
 
-        for _ in 0...31 - price.count {
+        for _ in 0...31 - price.bitWidth / 8 { //TODO double check
             //pad with zeros
             priceInWei.insert(0, at: 0)
         }
@@ -74,7 +80,7 @@ public class SignOrders {
             buffer.append(priceInWei[i])
         }
 
-        for i in 0...31 - expiryBuffer.count {
+        for _ in 0...31 - expiryBuffer.bitWidth / 8 {
             expiry.insert(0, at: 0)
         }
 
@@ -88,18 +94,24 @@ public class SignOrders {
             buffer.append(contractAddr[i])
         }
 
-        var ticketsUint8 = uInt16ArrayToUInt8(arrayOfUInt16: tickets)
+        var indices : [UInt16] = [UInt16]()
 
-        for i in 0...ticketsUint8.count - 1 {
-            buffer.append(ticketsUint8[i])
+        for i in 0...count {
+            let ticket : UInt16 = start + UInt16(i);
+            indices.append(ticket)
+        }
+
+        var uint8Indices = uInt16ArrayToUInt8(arrayOfUInt16: indices)
+
+        for i in 0...count {
+            buffer.append(uint8Indices[i])
         }
 
         return buffer
     }
 
-    func uInt16ArrayToUInt8(arrayOfUInt16: [UInt16]) -> [UInt8]
-    {
-        var arrayOfUint8 : [UInt8] = [UInt8]()
+    func uInt16ArrayToUInt8(arrayOfUInt16: [UInt16]) -> [UInt8] {
+        var arrayOfUint8 = [UInt8]()
         for i in 0...arrayOfUInt16.count - 1 {
             var UInt8ArrayPair = arrayOfUInt16[i].bigEndian.data.array
             arrayOfUint8.append(UInt8ArrayPair[0])
@@ -108,14 +120,13 @@ public class SignOrders {
         return arrayOfUint8
     }
 
-    func bufferToString(buffer : [UInt8]) -> String
-    {
-        var bufferString : String = "";
-        for i in 0...buffer.count - 1 {
-            bufferString += String(buffer[i])
+    func toByteArray<T>(_ value: T) -> [UInt8] {
+        var value = value
+        return withUnsafePointer(to: &value) {
+            $0.withMemoryRebound(to: UInt8.self, capacity: MemoryLayout<T>.size) {
+                Array(UnsafeBufferPointer(start: $0, count: MemoryLayout<T>.size))
+            }
         }
-        return bufferString
     }
-
 
 }
