efcbe72171ad6c1dc8776a1d35210576ff11d3c7
Oguzhan Gungor <cansingungor@gmail.com>
2018-02-24 17:24:10 +1000

    Adding a tableviewcell sublass for tickets

---
+++ Trust/Tokens/Views/TicketTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  TicketTableViewCell.swift
+//  Alpha-Wallet
+//
+//  Created by Oguzhan Gungor on 2/24/18.
+//  Copyright © 2018 Alpha-Wallet. All rights reserved.
+//
+
+import UIKit
+
+class TicketTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var ticketNumberLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var venueLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var seatLabel: UILabel!
+    @IBOutlet weak var zoneLabel: UILabel!
+
+}
