fdfa06497b8112ac442ceee7e99f7a0c774f0f0e
Oguzhan Gungor <cansingungor@gmail.com>
2018-02-24 18:33:53 +1000

    Adding a new cell to display ticket info

--- Trust/Tokens/ViewControllers/TicketsViewController.swift
+++ Trust/Tokens/ViewControllers/TicketsViewController.swift
@@ -13,7 +13,7 @@ import Result
 import TrustKeystore
 
 class TicketsViewController: UIViewController {
-    
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.navigationController?.applyTintAdjustment()
@@ -26,19 +26,30 @@ class TicketsViewController: UIViewController {
 
 extension TicketsViewController: UITableViewDelegate, UITableViewDataSource {
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        return 2
     }
-    
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return 1
+        }
         return 10
     }
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.section == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "TicketInfoCell", for: indexPath)
+            return cell
+        }
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "TicketCell", for: indexPath) as! TicketTableViewCell
         return cell
     }
 
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.section == 0 {
+            return 50
+        }
         return 90
     }
 }
