4bc79d4ec67f2a31ea3acd74a57d2b60284e39a4
Oguzhan Gungor <cansingungor@gmail.com>
2018-02-24 17:25:15 +1000

    Adding required tableview delegate and datasource methods

--- Trust/Tokens/ViewControllers/TicketsViewController.swift
+++ Trust/Tokens/ViewControllers/TicketsViewController.swift
@@ -23,3 +23,22 @@ class TicketsViewController: UIViewController {
         dismiss(animated: true, completion: nil)
     }
 }
+
+extension TicketsViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TicketCell", for: indexPath) as! TicketTableViewCell
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 90
+    }
+}
