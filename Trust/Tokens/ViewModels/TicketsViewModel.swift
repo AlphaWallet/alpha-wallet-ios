0787478f89592f056e92bfcc8e2ff1d4c6902c14
Oguzhan Gungor <cansingungor@gmail.com>
2018-02-24 21:31:27 +1000

    Adding TicketsViewModel

---
+++ Trust/Tokens/ViewModels/TicketsViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  TicketsViewModel.swift
+//  Alpha-Wallet
+//
+//  Created by Oguzhan Gungor on 2/24/18.
+//  Copyright © 2018 Alpha-Wallet. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct TicketsViewModel {
+
+    var tokens: [TokenObject] = []
+
+}
