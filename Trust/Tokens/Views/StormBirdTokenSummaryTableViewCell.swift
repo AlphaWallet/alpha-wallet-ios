4a7d4177fa23d0d518d767c20e6d6d9b6188ed33
Oguzhan Gungor <cansingungor@gmail.com>
2018-02-25 16:14:06 +1000

    Adding TableViewCell subclass to display stormbird token summary

---
+++ Trust/Tokens/Views/StormBirdTokenSummaryTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  StormBirdTokenSummaryTableViewCell.swift
+//  Alpha-Wallet
+//
+//  Created by Oguzhan Gungor on 2/24/18.
+//  Copyright © 2018 Alpha-Wallet. All rights reserved.
+//
+
+import UIKit
+
+class StormBirdTokenSummaryTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+
+    func configure(for token: TokenObject) {
+        countLabel.text = totalValidTicketNumber(for: token)
+        nameLabel.text = token.name.capitalized
+    }
+    
+    private func totalValidTicketNumber(for token: TokenObject) -> String {
+        let balance = token.balance
+        let validTickets = balance.filter { $0.balance > 0 }
+        return validTickets.count.toString()
+    }
+}
