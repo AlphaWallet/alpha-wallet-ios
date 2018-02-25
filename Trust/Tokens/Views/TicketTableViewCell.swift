64e24d0b18c50dd997546d7394834d082cd7a231
Oguzhan Gungor <cansingungor@gmail.com>
2018-02-25 19:45:23 +1000

    Updating UI

--- Trust/Tokens/Views/TicketTableViewCell.swift
+++ Trust/Tokens/Views/TicketTableViewCell.swift
@@ -17,4 +17,12 @@ class TicketTableViewCell: UITableViewCell {
     @IBOutlet weak var seatLabel: UILabel!
     @IBOutlet weak var zoneLabel: UILabel!
 
+    func configure(ticketHolder: TicketHolder) {
+        ticketNumberLabel.text = ticketHolder.ticketCount
+        nameLabel.text = ticketHolder.name
+        venueLabel.text = ticketHolder.venue
+        dateLabel.text = ticketHolder.date.format("dd MMM yyyy")
+        zoneLabel.text = ticketHolder.zone
+        seatLabel.text = ticketHolder.seatRange
+    }
 }
