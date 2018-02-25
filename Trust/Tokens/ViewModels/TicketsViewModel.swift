cc612c44a3c237e0d54f7ac89b6c528032aecc17
Oguzhan Gungor <cansingungor@gmail.com>
2018-02-25 19:46:36 +1000

    Updating TicketsViewModel to show real data

--- Trust/Tokens/ViewModels/TicketsViewModel.swift
+++ Trust/Tokens/ViewModels/TicketsViewModel.swift
@@ -11,17 +11,48 @@ import UIKit
 
 struct TicketsViewModel {
 
-    var token: TokenObject
+    let token: TokenObject
+    let ticketHolders: [TicketHolder]
+
+    func item(for indexPath: IndexPath) -> TicketHolder {
+        return ticketHolders[indexPath.row]
+    }
+
+    func cell(for tableView: UITableView, indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.section == 0 {
+            return summaryCell(for: tableView, indexPath: indexPath)
+        }
+        return ticketCell(for: tableView, indexPath: indexPath)
+    }
 
     var numberOfSections: Int {
         return 2
     }
-    
+
     func numberOfItems(for section: Int) -> Int {
         if section == 0 {
             return 1
         }
-        return token.balance.count
+        return ticketHolders.count
     }
 
+    func height(for section: Int) -> CGFloat {
+        if section == 0 {
+            return 50
+        }
+        return 90
+    }
+    
+    private func summaryCell(for tableView: UITableView, indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "StormBirdTokenSummaryTableViewCell", for: indexPath) as! StormBirdTokenSummaryTableViewCell
+        cell.configure(for: token)
+        return cell
+    }
+
+    private func ticketCell(for tableView: UITableView, indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TicketCell", for: indexPath) as! TicketTableViewCell
+        let ticketHolder = item(for: indexPath)
+        cell.configure(ticketHolder: ticketHolder)
+        return cell
+    }
 }
