ea11fe00ba7706c04599784b893b8b1acf34e773
Oguzhan Gungor <cansingungor@gmail.com>
2018-02-28 20:39:38 +1000

    Following the existing architecture in Tickets VC

--- Trust/Tokens/ViewControllers/TicketsViewController.swift
+++ Trust/Tokens/ViewControllers/TicketsViewController.swift
@@ -12,9 +12,20 @@ import StatefulViewController
 import Result
 import TrustKeystore
 
+protocol TicketsViewControllerDelegate: class {
+    func didPressRedeem(token: TokenObject, in viewController: UIViewController)
+    func didPressSell(token: TokenObject, in viewController: UIViewController)
+    func didPressTransfer(ticketHolder: TicketHolder?, token: TokenObject, in viewController: UIViewController)
+}
+
 class TicketsViewController: UIViewController {
 
     var viewModel: TicketsViewModel!
+    var tokensStorage: TokensDataStore!
+    var account: Wallet!
+    var dataCoordinator: TransactionDataCoordinator!
+    var session: WalletSession!
+    weak var delegate: TicketsViewControllerDelegate?
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -23,6 +34,18 @@ class TicketsViewController: UIViewController {
 
     @IBAction func didTapDoneButton(_ sender: UIBarButtonItem) {
         dismiss(animated: true, completion: nil)
+    }
+
+    @IBAction func didPressRedeem(_ sender: UIButton) {
+        delegate?.didPressRedeem(token: viewModel.token, in: self)
+    }
+
+    @IBAction func didPressSell(_ sender: UIButton) {
+        delegate?.didPressSell(token: viewModel.token, in: self)
+    }
+
+    @IBAction func didPressTransfer(_ sender: UIButton) {
+        delegate?.didPressTransfer(ticketHolder: nil, token: viewModel.token, in: self)
     }
 }
 
@@ -41,5 +64,13 @@ extension TicketsViewController: UITableViewDelegate, UITableViewDataSource {
 
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return viewModel.height(for: indexPath.section)
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if viewModel.ticketCellPressed(for: indexPath) {
+            let ticketHolder = viewModel.item(for: indexPath)
+            delegate?.didPressTransfer(ticketHolder: ticketHolder, token: viewModel.token, in: self)
+            tableView.deselectRow(at: indexPath, animated: true)
+        }
     }
 }
