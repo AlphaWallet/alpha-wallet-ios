cd567d383e401bfdf73d03c431732b2f8c8bef1a
James Sangalli <j.l.sangalli@gmail.com>
2018-03-03 10:42:08 +0800

    redeem signing now working but with fake account

--- Trust/Redeem/CreateRedeem.swift
+++ Trust/Redeem/CreateRedeem.swift
@@ -3,6 +3,7 @@
 //
 
 import Foundation
+import BigInt
 import TrustKeystore
 
 class CreateRedeem {
@@ -10,13 +11,37 @@ class CreateRedeem {
     private let keyStore = try! EtherKeystore()
 
     func generateRedeem(ticketIndices: [UInt16], account: Account) -> [Data] {
+        //TODO remove after testing
+        let accountTest = keyStore.createAccount(password: "test")
+
         var messageAndSignature = [Data]()
-        let message = SignOrders.uInt16ArrayToUInt8(arrayOfUInt16: ticketIndices)
-        let signature = keyStore.signMessage(Data(bytes: message), for: account)
+        var message = [UInt8]()
+        var timestamp = generateTimeStamp()
+        let ticketCount = ticketIndices.count * 2
+        message.reserveCapacity(ticketCount + timestamp.count)
+        message = SignOrders.uInt16ArrayToUInt8(arrayOfUInt16: ticketIndices)
+        for i in 0...timestamp.count - 1 {
+            message.append(timestamp[i])
+        }
+        let signature = keyStore.signMessageData(Data(bytes: message), for: accountTest)
         messageAndSignature.append(Data(bytes: message))
         try! messageAndSignature.append(signature.dematerialize())
+
+        print(try! "signature: " + signature.dematerialize().hexString)
 
         return messageAndSignature
     }
 
+    //optimised for decimal
+    func parseToQR(_ data: Data) -> BigInt {
+        return BigInt.init(data.hexString, radix: 16)!
+    }
+    
+    func generateTimeStamp() -> [UInt8] {
+        let time = Date().timeIntervalSince1970.binade
+        let minsTime = (time / 30).binade
+        let minsTimeBigUInt = BigUInt(minsTime)
+        return Array(minsTimeBigUInt.serialize())
+    }
+    
 }
