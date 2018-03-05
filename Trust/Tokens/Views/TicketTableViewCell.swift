0bbb209055ad06b8ef4734976fc59c97503a453f
Oguzhan Gungor <cansingungor@gmail.com>
2018-03-05 22:12:39 +1000

    Using the reusable view in ticket table view cell

--- Trust/Tokens/Views/TicketTableViewCell.swift
+++ Trust/Tokens/Views/TicketTableViewCell.swift
@@ -9,20 +9,9 @@
 import UIKit
 
 class TicketTableViewCell: UITableViewCell {
-
-    @IBOutlet weak var ticketNumberLabel: UILabel!
-    @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var venueLabel: UILabel!
-    @IBOutlet weak var dateLabel: UILabel!
-    @IBOutlet weak var seatLabel: UILabel!
-    @IBOutlet weak var zoneLabel: UILabel!
+    @IBOutlet weak var ticketView: TicketView!
 
     func configure(ticketHolder: TicketHolder) {
-        ticketNumberLabel.text = ticketHolder.ticketCount
-        nameLabel.text = ticketHolder.name
-        venueLabel.text = ticketHolder.venue
-        dateLabel.text = ticketHolder.date.format("dd MMM yyyy")
-        zoneLabel.text = ticketHolder.zone
-        seatLabel.text = ticketHolder.seatRange
+        ticketView.configure(ticketHolder: ticketHolder)
     }
 }
