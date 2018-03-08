053e2674ee3cb38cad53ddbec5158cd91a37d927
Hwee-Boon Yar <hboon@motionobj.com>
2018-03-08 22:13:26 +0800

    Add Wallet Tab (duplicated and modified from Tokens tab)

---
+++ Trust/AlphaWallet/ViewModels/AlphaWalletTokenViewCellViewModel.swift
@@ -0,0 +1,63 @@
+// Copyright © 2018 Stormbird PTE. LTD.
+
+import Foundation
+import UIKit
+import BigInt
+
+struct AlphaWalletTokenViewCellViewModel {
+    private let shortFormatter = EtherNumberFormatter.short
+    private let token: TokenObject
+    let ticker: CoinTicker?
+
+    init(
+        token: TokenObject,
+        ticker: CoinTicker?
+    ) {
+        self.token = token
+        self.ticker = ticker
+    }
+
+    var title: String {
+        return token.title
+    }
+
+    var amount: String {
+        return shortFormatter.string(from: BigInt(token.value) ?? BigInt(), decimals: token.decimals)
+    }
+
+    var issuer: String {
+        return ""
+    }
+
+    var blockChainName: String {
+        return "Ethereum Blockchain"
+    }
+
+    var backgroundColor: UIColor {
+        return Colors.appBackground
+    }
+
+    var contentsBackgroundColor: UIColor {
+        return Colors.appWhite
+    }
+
+    var titleColor: UIColor {
+        return Colors.appText
+    }
+
+    var subtitleColor: UIColor {
+        return Colors.appBackground
+    }
+
+    var titleFont: UIFont {
+        return Fonts.light(size: 25)!
+    }
+
+    var subtitleFont: UIFont {
+        return Fonts.semibold(size: 10)!
+    }
+
+    var cellHeight: CGFloat {
+        return 130
+    }
+}
