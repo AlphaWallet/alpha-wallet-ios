6599bacf42ea0960ecfbb33397522d4eacb4de5d
Oguzhan Gungor <cansingungor@gmail.com>
2018-03-08 22:39:54 +1000

    Adding a new helper class to convert a signature to hex or decimal

---
+++ Trust/Redeem/Helpers/SignatureHelper.swift
@@ -0,0 +1,29 @@
+//
+//  SignatureHelper.swift
+//  Alpha-Wallet
+//
+//  Created by Oguzhan Gungor on 3/8/18.
+//  Copyright © 2018 Alpha-Wallet. All rights reserved.
+//
+
+import Foundation
+import TrustKeystore
+import BigInt
+
+class SignatureHelper {
+
+    class func signatureAsHex(for message: String, account: Account) -> String? {
+        let keystore = try! EtherKeystore()
+        let signature = keystore.signMessageData(message.data(using: String.Encoding.utf8), for: account)
+        let signatureHex = try? signature.dematerialize().toHex(options: .upperCase)
+        guard let data = signatureHex else {
+            return nil
+        }
+        return data
+    }
+
+    class func signatureAsDecimal(for message: String, account: Account) -> String? {
+        let signatureHex = signatureAsHex(for: message, account: account)!
+        return BigInt(signatureHex, radix: 16)!.description
+    }
+}
