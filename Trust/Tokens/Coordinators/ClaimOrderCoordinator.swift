ed2dccafd6436999e89b5784e69c8e7440bbe68e
Oguzhan Gungor <cansingungor@gmail.com>
2018-03-10 19:29:28 +1000

    Fixing completionHandler return type

--- Trust/Tokens/Coordinators/ClaimOrderCoordinator.swift
+++ Trust/Tokens/Coordinators/ClaimOrderCoordinator.swift
@@ -25,18 +25,18 @@ class ClaimOrderCoordinator {
                     v: UInt8,
                     r: String,
                     s: String,
-                    completion: @escaping (Any) -> Void//Result<Void, AnyError>) -> Void
-    ) {
+                    completion: @escaping (Result<Any, AnyError>) -> Void
+        ) {
         let request = ClaimStormBirdOrder(expiry: expiry, indices: indices, v: v, r: r, s: s)
         web3.request(request: request) { result in
             switch result {
             //TODO handle cases for UI
             case .success(let res):
                 print(res)
-                completion(res)
+                completion(.success(res))
             case .failure(let err):
                 print(err)
-                completion(err)
+                completion(.failure(AnyError(err)))
             }
         }
     }
