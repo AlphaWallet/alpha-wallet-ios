772cde06ea3e9cb6e4b16a4a49e8666fff2e6143
Oguzhan Gungor <cansingungor@gmail.com>
2018-03-10 19:28:29 +1000

    Updating test code for claming order

--- Trust/Vendors/New Group/Commands/ContractStormBirdTransfer.swift
+++ Trust/Vendors/New Group/Commands/ContractStormBirdTransfer.swift
@@ -7,7 +7,7 @@ struct ContractStormBirdTransfer: Web3Request {
     let indices: [UInt16]
     
     var type: Web3RequestType {
-        let abi = "{\"constant\":false,\"inputs\":[{\"name\":\"_to\",\"type\":\"address\"},{\"name\":\"ticketIndices\",\"type\":\"uint16[]\"}],\"name\":\"transfer\",\"outputs\":[],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"function\"} , [\"\(address)\", \(indices)]"
+        let abi = "{\"constant\":false,\"inputs\":[{\"name\":\"_to\",\"type\":\"address\"},{\"name\":\"ticketIndices\",\"type\":\"uint16[]\"}],\"name\":\"transfer\",\"outputs\":[],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"function\"}, [\"\(address)\", \(indices)]"
         let run = "web3.eth.abi.encodeFunctionCall(" + abi + ")"
         return .script(command: run)
     }
