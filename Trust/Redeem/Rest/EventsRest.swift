8643222cfb4b5d39ca6d81d4c8e59b50cad1d1dd
Oguzhan Gungor <cansingungor@gmail.com>
2018-03-11 19:45:37 +1000

    Adding a rest client for events

---
+++ Trust/Redeem/Rest/EventsRest.swift
@@ -0,0 +1,34 @@
+//
+//  EventsRest.swift
+//  Alpha-Wallet
+//
+//  Created by Oguzhan Gungor on 3/11/18.
+//  Copyright © 2018 Alpha-Wallet. All rights reserved.
+//
+
+import Foundation
+import Result
+
+public class EventsRest {
+
+    func getEvents(completion: @escaping (Result<[Event], AnyError>) -> Void) {
+        RestClient.get(endPoint: EndPoints.EventBaseUrl, completion: { response in
+            print(response)
+            guard let statusCode = response.response?.statusCode else {
+                completion(.failure(AnyError(response.error!)))
+                return
+            }
+            guard let jsonData = response.data else {
+                completion(.failure(AnyError(RestError.invalidResponse("JSON is invalid"))))
+                return
+            }
+
+            if 200...299 ~= statusCode { // success
+                let events: [Event] = Event.from(data: jsonData)!
+                completion(.success(events))
+            } else {
+                completion(.failure(AnyError(RestError.invalidResponse("Could not parse data"))))
+            }
+        })
+    }
+}
