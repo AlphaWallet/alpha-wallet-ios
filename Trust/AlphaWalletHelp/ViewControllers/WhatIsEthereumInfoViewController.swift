c40eeabc19c17fe9c8d2bfb6218931bf8e49a357
Hwee-Boon Yar <hboon@motionobj.com>
2018-03-11 17:45:41 +0800

    Add Help Tab and placeholder help contents

---
+++ Trust/AlphaWalletHelp/ViewControllers/WhatIsEthereumInfoViewController.swift
@@ -0,0 +1,9 @@
+// Copyright © 2018 Stormbird PTE. LTD.
+
+import UIKit
+
+class WhatIsEthereumInfoViewController: AlphaWalletHelpContentsViewController{
+    override func url() -> URL? {
+        return R.file.whatIsEthereumInfoHtml()
+    }
+}
