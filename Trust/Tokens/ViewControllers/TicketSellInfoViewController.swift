59004158dc9e1b1462eea92b6c2815c8729d5590
Hwee-Boon Yar <hboon@motionobj.com>
2018-03-11 20:25:16 +0800

    Add sell info screen (not displayed yet)

---
+++ Trust/Tokens/ViewControllers/TicketSellInfoViewController.swift
@@ -0,0 +1,9 @@
+// Copyright © 2018 Stormbird PTE. LTD.
+
+import UIKit
+
+class TicketSellInfoViewController: AlphaWalletHelpContentsViewController{
+    override func url() -> URL? {
+        return R.file.sellInfoHtml()
+    }
+}
