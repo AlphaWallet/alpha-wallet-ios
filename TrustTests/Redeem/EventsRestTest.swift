5825009a16b6354966f4fd8a75a94c8421941eb8
James Sangalli <j.l.sangalli@gmail.com>
2018-03-14 12:55:17 +0800

    remove test as it is not useful

--- TrustTests/Redeem/EventsRestTest.swift
+++ TrustTests/Redeem/EventsRestTest.swift
@@ -15,18 +15,18 @@ class EventsRestTest: XCTestCase {
     var expectations = [XCTestExpectation]()
 
     func testEventsRest() {
-        let expectation = self.expectation(description: "wait til callback")
-        expectations.append(expectation)
-        EventsRest().getEvents { result in
-            switch result {
-            case .success(let events):
-                print(events)
-                expectation.fulfill()
-            case .failure(let error):
-                print(error)
-            }
-        }
-        wait(for: expectations, timeout: 10)
+//        let expectation = self.expectation(description: "wait til callback")
+//        expectations.append(expectation)
+//        EventsRest().getEvents { result in
+//            switch result {
+//            case .success(let events):
+//                print(events)
+//                expectation.fulfill()
+//            case .failure(let error):
+//                print(error)
+//            }
+//        }
+//        wait(for: expectations, timeout: 10)
     }
 
 }
