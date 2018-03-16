f1281c79aed2f2da29b502426748bb195a241b83
Hwee-Boon Yar <hboon@motionobj.com>
2018-03-16 12:57:20 +0800

    Style create passcode screen

---
+++ Trust/AlphaWalletLock/Views/AlphaWalletLockView.swift
@@ -0,0 +1,70 @@
+// Copyright SIX DAY LLC. All rights reserved.
+// Copyright © 2018 Stormbird PTE. LTD.
+
+import UIKit
+
+//Duplicated from AlphaWalletLockView.swift for easier upstream merging
+class AlphaWalletLockView: UIView {
+	var characterView = UIStackView()
+	var lockTitle = UILabel()
+	var model: LockViewModel!
+	var characters: [AlphaWalletPasscodeCharacterView]!
+	init(_ model: LockViewModel) {
+		super.init(frame: CGRect.zero)
+		self.model = model
+		self.characters = passcodeCharacters()
+		configCharacterView()
+		configLabel()
+		addUiElements()
+		applyConstraints()
+	}
+	private func configCharacterView() {
+		characterView = UIStackView(arrangedSubviews: self.characters)
+		characterView.axis = .horizontal
+		characterView.distribution = .fillEqually
+		characterView.alignment = .fill
+		characterView.spacing = 20
+		characterView.translatesAutoresizingMaskIntoConstraints = false
+	}
+	private func configLabel() {
+		lockTitle.font = Fonts.light(size: 18)
+		lockTitle.textAlignment = .center
+		lockTitle.translatesAutoresizingMaskIntoConstraints = false
+	}
+	private func applyConstraints() {
+		characterView.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+		characterView.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+		characterView.heightAnchor.constraint(equalToConstant: 20.0).isActive = true
+		lockTitle.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+		lockTitle.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+		lockTitle.bottomAnchor.constraint(equalTo: characterView.topAnchor, constant: -20).isActive = true
+	}
+	private func addUiElements() {
+		self.backgroundColor = UIColor.white
+		self.addSubview(lockTitle)
+		self.addSubview(characterView)
+	}
+	private func passcodeCharacters() -> [AlphaWalletPasscodeCharacterView] {
+		var characters = [AlphaWalletPasscodeCharacterView]()
+		for _ in 0..<model.charCount() {
+			let passcodeCharacterView = AlphaWalletPasscodeCharacterView()
+			passcodeCharacterView.heightAnchor.constraint(equalToConstant: 20.0).isActive = true
+			passcodeCharacterView.widthAnchor.constraint(equalToConstant: 20).isActive = true
+			characters.append(passcodeCharacterView)
+		}
+		return characters
+	}
+	func shake() {
+		let keypath = "position"
+		let animation = CABasicAnimation(keyPath: keypath)
+		animation.duration = 0.07
+		animation.repeatCount = 4
+		animation.autoreverses = true
+		animation.fromValue = NSValue(cgPoint: CGPoint(x: characterView.center.x - 10, y: characterView.center.y))
+		animation.toValue = NSValue(cgPoint: CGPoint(x: characterView.center.x + 10, y: characterView.center.y))
+		characterView.layer.add(animation, forKey: keypath)
+	}
+	required init?(coder aDecoder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+}
