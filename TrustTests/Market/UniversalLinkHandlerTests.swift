4082ace1577e1e3849660823239dad711b192d04
James Sangalli <j.l.sangalli@gmail.com>
2018-03-27 19:53:30 +0800

    refactor link handling class and start writing encoder for link

--- TrustTests/Market/UniversalLinkHandlerTests.swift
+++ TrustTests/Market/UniversalLinkHandlerTests.swift
@@ -11,16 +11,10 @@ class UniversalLinkHandlerTests: XCTestCase {
     func testUniversalLinkParser() {
         let testUrl = "https://app.awallet.io/AA9CQAAAAAC8mhAmpLxvC6i75IbR0J2lcys55IDwAoKW" +
                 "hOr6d7KY+8hNPvg7BADzHG9wEdp+nfFuaoeZzZ5PskIO9ym+0j0VxiTh6uyb7et3F/Y2HcgvcqNTA0xZ1gE="
-
-        let signedOrder: SignedOrder = UniversalLinkHandler.parseURL(url: testUrl)
-
-        print(signedOrder.signature)
+        let signedOrder = UniversalLinkHandler.parseURL(url: testUrl)
         XCTAssertGreaterThanOrEqual(signedOrder.signature.count, 130)
-
         let url = UniversalLinkHandler.createUniversalLink(signedOrder: signedOrder)
-
         print(url)
-
         XCTAssertEqual(testUrl, url)
     }
 
