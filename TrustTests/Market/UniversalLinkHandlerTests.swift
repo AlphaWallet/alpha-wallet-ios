4e65cad46020a5603f4fa5434dd3faeef73df852
James Sangalli <j.l.sangalli@gmail.com>
2018-03-28 23:18:53 +0800

    parsed link properly according to example

--- TrustTests/Market/UniversalLinkHandlerTests.swift
+++ TrustTests/Market/UniversalLinkHandlerTests.swift
@@ -9,9 +9,7 @@ import XCTest
 class UniversalLinkHandlerTests: XCTestCase {
 
     func testUniversalLinkParser() {
-        let testUrl = "https://app.awallet.io/AA9CQAAAAAC8mhAmpLxvC6i75IbR0J2lcys55IDwAoKWhOr6d7KY+8hNPvg7BADzHG9wEdp+nfFuaoeZzZ5PskIO9ym+0j0VxiTh6uyb7et3F/Y2HcgvcqNTA0xZ1gE="
-//                "https://app.awallet.io/AA9CQFq1tAAAe+6CvdnoZrK9EUeApH8iYcaE4wECAwQFBgcICS+YK4TGNZZ6m2MG7V" +
-//                "eJp8GRkWQXHjfczfS1m+VHVEEFMIGLiWt9JA9WxZ698gkGLuVNp6NZCQVzlnTc/c7PPpsb"
+        let testUrl = "https://app.awallet.io/AA9CQFq1tAAAe+6CvdnoZrK9EUeApH8iYcaE4wECAwQFBgcICS+YK4TGNZZ6m2MG7VeJp8GRkWQXHjfczfS1m+VHVEEFMIGLiWt9JA9WxZ698gkGLuVNp6NZCQVzlnTc/c7PPpsb"
         let signedOrder = UniversalLinkHandler.parseURL(url: testUrl)
         XCTAssertGreaterThanOrEqual(signedOrder.signature.count, 130)
         //let url = UniversalLinkHandler.createUniversalLink(signedOrder: signedOrder)
