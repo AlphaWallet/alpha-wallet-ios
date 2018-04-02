da33045d7ae9d36b0850a1de81039961af87bd83
James Sangalli <j.l.sangalli@gmail.com>
2018-04-02 11:20:38 +0800

    handle renaming for review of PR

--- TrustTests/Market/UniversalLinkHandlerTests.swift
+++ TrustTests/Market/UniversalLinkHandlerTests.swift
@@ -10,11 +10,11 @@ class UniversalLinkHandlerTests: XCTestCase {
 
     func testUniversalLinkParser() {
         let testUrl = "https://app.awallet.io/AAGGoFq8Ule8mhAmpLxvC6i75IbR0J2lcys55AECAwQFBgcICYvWi5I+Tl5m9XumBD5jLIm6i39kD7F40UW4BaJDEVOWLTYz3kek7wjT7Bn+2w0NCiyx7zWuvseTA8qfoIqCIxob"
-        let signedOrder = UniversalLinkHandler.parseURL(url: testUrl)
+        let signedOrder = UniversalLinkHandler().parseURL(url: testUrl)
         XCTAssertGreaterThanOrEqual(signedOrder.signature.count, 130)
-        //let url = UniversalLinkHandler.createUniversalLink(signedOrder: signedOrder)
-        //print(url)
-        //XCTAssertEqual(testUrl, url)
+        let url = UniversalLinkHandler().createUniversalLink(signedOrder: signedOrder)
+        print(url)
+        XCTAssertEqual(testUrl, url)
     }
 
 }
