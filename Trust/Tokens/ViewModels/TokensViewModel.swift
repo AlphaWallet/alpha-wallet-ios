55ca0e0ea0ea3dd9061b1c5567598e3820773e8e
Hwee-Boon Yar <hboon@motionobj.com>
2018-04-02 13:21:38 +0800

    Add the FIFA token if it is not in the list

--- Trust/Tokens/ViewModels/TokensViewModel.swift
+++ Trust/Tokens/ViewModels/TokensViewModel.swift
@@ -108,8 +108,7 @@ struct TokensViewModel {
 
     //FIFA make the FIFA token be index 1. Can remove the function and replace with the argument when we no longer need this
     private func reorderTokensSoFIFAAtIndex1(tokens: [TokenObject]) -> [TokenObject] {
-        let index = tokens.index { $0.address.eip55String == Constants.fifaContractAddress
-        }
+        let index = tokens.index { $0.address.eip55String == Constants.fifaContractAddress }
         if let index = index, tokens.count >= 2 {
             var reorderedTokens = tokens
             let target = reorderedTokens[index]
