031054ef8b91a4683b096a74ea3de469cd2ab558
James Sangalli <j.l.sangalli@gmail.com>
2018-04-05 12:07:00 +0800

    with test to create link

--- TrustTests/Market/UniversalLinkHandlerTests.swift
+++ TrustTests/Market/UniversalLinkHandlerTests.swift
@@ -5,9 +5,11 @@
 import Foundation
 @testable import Trust
 import XCTest
+import BigInt
+import TrustKeystore
 
 class UniversalLinkHandlerTests: XCTestCase {
-
+    
     func testUniversalLinkParser() {
         let testUrl = "https://app.awallet.io/AAGGoFq8Ule8mhAmpLxvC6i75IbR0J2lcys55AECAwQFBgcICYvWi5I+Tl5m9XumBD5jLIm6i39kD7F40UW4BaJDEVOWLTYz3kek7wjT7Bn+2w0NCiyx7zWuvseTA8qfoIqCIxob"
         let signedOrder = UniversalLinkHandler().parseURL(url: testUrl)
@@ -15,6 +17,26 @@ class UniversalLinkHandlerTests: XCTestCase {
         let url = UniversalLinkHandler().createUniversalLink(signedOrder: signedOrder)
         print(url)
         XCTAssertEqual(testUrl, url)
+    }
+    
+    func testCreateUniversalLink() {
+        var indices = [UInt16]()
+        indices.append(1)
+        
+        let testOrder1 = Order(price: BigUInt("0")!,
+                               indices: indices,
+                               expiry: BigUInt("0")!,
+                               contractAddress: "0xa3347C8ED134444C25169872c90a6360D42b3691",
+                               start: BigUInt("91239231313")!,
+                               count: 3
+        )
+        
+        var testOrders = [Order]()
+        testOrders.append(testOrder1)
+        //let account = try! EtherKeystore().getAccount(for: Address(string: "0x007bEe82BDd9e866b2bd114780a47f2261C684E3")!)
+        //let signedOrder = try! OrderHandler().signOrders(orders: testOrders, account: account!)
+        //let url = UniversalLinkHandler().createUniversalLink(signedOrder: signedOrder[0])
+        //print(url)
     }
 
 }
