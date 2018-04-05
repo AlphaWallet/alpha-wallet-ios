1491d8ed8b016a6ee3dc9df681f327b52d49b39a
Hwee-Boon Yar <hboon@motionobj.com>
2018-04-05 10:53:43 +0800

    Undo additional height for buttons at the bottom for iPhone X

--- Trust/Redeem/ViewControllers/QuantitySelectionViewController.swift
+++ Trust/Redeem/ViewControllers/QuantitySelectionViewController.swift
@@ -101,7 +101,7 @@ class QuantitySelectionViewController: UIViewController {
 
             footerBar.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             footerBar.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            footerBar.topAnchor.constraint(equalTo: view.layoutGuide.bottomAnchor, constant: -buttonsHeight),
+            footerBar.heightAnchor.constraint(equalToConstant: buttonsHeight),
             footerBar.bottomAnchor.constraint(equalTo: view.bottomAnchor),
         ])
     }
