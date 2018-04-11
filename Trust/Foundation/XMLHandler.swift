1d6c76b58888fdc00aaaa15eba7a14b4e8624a42
James Sangalli <j.l.sangalli@gmail.com>
2018-04-11 16:13:09 +0800

    handles xml parsing for asset definition

--- Trust/Foundation/XMLHandler.swift
+++ Trust/Foundation/XMLHandler.swift
@@ -10,7 +10,6 @@ import SwiftyXMLParser
 
 struct FIFAInfo {
     let locale: String
-    let team: String
     let venue: String
     let time: Int
     let countryA: Int
@@ -20,24 +19,29 @@ struct FIFAInfo {
     let number: Int
 }
 
+/**
+ langs:
+ 0 = ru
+ 1 = en
+ 2 = zh
+ 3 = es
+ */
+
 public class XMLHandler {
     
     let xml = try! XML.parse(AssetDefinitionXML.assetDefinition)
     
-    func getFifaInfoForToken(tokenId: String, lang: String) -> FIFAInfo {
+    func getFifaInfoForToken(tokenId: String, lang: Int) -> FIFAInfo {
         let locale = getLocale(attribute: tokenId.substring(to: 2), lang: lang)
-        let team = getTeam(attribute: tokenId.substring(to: 8), lang: lang)
-        let venue = getVenue(attribute: tokenId.substring(to: 6), lang: lang)
-        let time = Int(tokenId.substring(with: Range((5, 12))), radix: 16)!
-        let countryA = Int(tokenId.substring(with: Range(13, 19)), radix: 16)!
-        let countryB = Int(tokenId.substring(with: Range(20, 26)), radix: 16)!
-        let match = Int(tokenId.substring(with: Range(27, 29)), radix: 16)!
-        let category = Int(tokenId.substring(with: Range(30, 32)), radix: 16)!
-        let number = Int(tokenId.substring(from: 33), radix: 16)!
-        return FIFAInfo
-        (
+        let venue = getVenue(attribute: tokenId.substring(with: Range(uncheckedBounds: (2, 4))), lang: lang)
+        let time = Int(tokenId.substring(with: Range(uncheckedBounds: (5, 12))), radix: 16)!
+        let countryA = Int(tokenId.substring(with: Range(uncheckedBounds: (13, 18))), radix: 16)!
+        let countryB = Int(tokenId.substring(with: Range(uncheckedBounds: (19, 24))), radix: 16)!
+        let match = Int(tokenId.substring(with: Range(uncheckedBounds: (25, 26))), radix: 16)!
+        let category = Int(tokenId.substring(with: Range(uncheckedBounds: (27, 29))), radix: 16)!
+        let number = Int(tokenId.substring(from: 30), radix: 16)!
+        return FIFAInfo(
                         locale: locale,
-                        team: team,
                         venue: venue,
                         time: time,
                         countryA: countryA,
@@ -48,14 +52,14 @@ public class XMLHandler {
         )
     }
     
-    func getLocale(attribute: String, lang: String) -> String {
+    func getLocale(attribute: String, lang: Int) -> String {
         let localeNumber = Int(attribute, radix: 16)!
-        return xml["field"]["locality"]["mapping"]["entity"][localeNumber][lang]
+        return xml["asset"]["fields"]["field"][0][0]["mapping"]["entity"][localeNumber]["name"][lang].text!
     }
     
-    func getVenue(attribute: String, lang: String) -> String {
+    func getVenue(attribute: String, lang: Int) -> String {
         let venueNumber = Int(attribute, radix: 16)!
-        return xml["field"]["venue"]["mapping"]["entity"][venueNumber][lang]
+        return xml["asset"]["fields"]["field"][1][0]["mapping"]["entity"][venueNumber]["name"][lang].text!
     }
     
 }
