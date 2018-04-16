8b88828a9633df78a8d27167e8a315131ee75eea
James Sangalli <j.l.sangalli@gmail.com>
2018-04-16 23:27:27 +0800

    with updated help and faq

--- Trust/AlphaWalletHelp/ViewControllers/WhyUseEthereumInfoViewController.swift
+++
@@ -1,9 +0,0 @@
-// Copyright © 2018 Stormbird PTE. LTD.
-
-import UIKit
-
-class WhyUseEthereumInfoViewController: HelpContentsViewController {
-    override func url() -> URL? {
-        return R.file.whyUseEthereumInfoHtml()
-    }
-}
