fbe07a55a05275be5e84bb2a549ff59eb9a26dc8
Hwee-Boon Yar <hboon@motionobj.com>
2018-04-30 11:37:22 +0800

    Fix: localizable string that has wrong argument type

--- Trust/Transfer/Types/ConfigureTransactionError.swift
+++ Trust/Transfer/Types/ConfigureTransactionError.swift
@@ -10,10 +10,7 @@ enum ConfigureTransactionError: LocalizedError {
     var errorDescription: String? {
         switch self {
         case .gasLimitTooHigh:
-            return String(
-                format: NSLocalizedString( "configureTransaction.error.gasLimitTooHigh", value: "Gas Limit too high. Max available: %d", comment: ""),
-                ConfigureTransaction.gasLimitMax
-            )
+            return R.string.localizable.configureTransactionErrorGasLimitTooHigh(ConfigureTransaction.gasLimitMax)
         case .gasFeeTooHigh:
             return R.string.localizable.configureTransactionErrorGasFeeTooHigh(String(ConfigureTransaction.gasFeeMax))
         }
