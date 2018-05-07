c61f5a187418bf6d4af5f7d74fd0270b25479802
Hwee-Boon Yar <hboon@motionobj.com>
2018-05-07 13:52:22 +0800

    Reorder tabs to: Marketplace, Wallet, Transactions, Settings

--- TrustTests/Coordinators/InCoordinatorTests.swift
+++ TrustTests/Coordinators/InCoordinatorTests.swift
@@ -22,9 +22,9 @@ class InCoordinatorTests: XCTestCase {
         XCTAssertNotNil(tabbarController)
 
         XCTAssert(tabbarController?.viewControllers!.count == 4)
-        XCTAssert((tabbarController?.viewControllers?[0] as? UINavigationController)?.viewControllers[0] is TransactionsViewController)
+        XCTAssert((tabbarController?.viewControllers?[0] as? UINavigationController)?.viewControllers[0] is MarketplaceViewController)
         XCTAssert((tabbarController?.viewControllers?[1] as? UINavigationController)?.viewControllers[0] is TokensViewController)
-        XCTAssert((tabbarController?.viewControllers?[2] as? UINavigationController)?.viewControllers[0] is MarketplaceViewController)
+        XCTAssert((tabbarController?.viewControllers?[2] as? UINavigationController)?.viewControllers[0] is TransactionsViewController)
         XCTAssert((tabbarController?.viewControllers?[3] as? UINavigationController)?.viewControllers[0] is SettingsViewController)
     }
 
