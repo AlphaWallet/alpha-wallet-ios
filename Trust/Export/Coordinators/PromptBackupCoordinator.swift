72c450ec6b9de0a10829058c21a2fc8994249b0f
Hwee-Boon Yar <hboon@motionobj.com>
2018-05-17 13:50:50 +0800

    Fix: was prompting to backup a wallets with non-zero balance *after* switching from them during the same app session. Specifically. after creating or importing another account

--- Trust/Export/Coordinators/PromptBackupCoordinator.swift
+++ Trust/Export/Coordinators/PromptBackupCoordinator.swift
@@ -7,9 +7,15 @@ protocol PromptBackupCoordinatorDelegate: class {
     func didFinish(in coordinator: PromptBackupCoordinator)
 }
 
+///We allow user to switch wallets, so it's important to know which wallet we are prompting for. It might not be the current wallet
 class PromptBackupCoordinator: Coordinator {
     var coordinators: [Coordinator] = []
     weak var delegate: PromptBackupCoordinatorDelegate?
+    var walletAddress: String
+
+    init(walletAddress: String) {
+        self.walletAddress = walletAddress
+    }
 
     func start() {
         let keystore = try! EtherKeystore()
@@ -19,7 +25,7 @@ class PromptBackupCoordinator: Coordinator {
         }
         let coordinator = WalletCoordinator(keystore: keystore)
         coordinator.delegate = self
-        let proceed = coordinator.start(.backupWallet)
+        let proceed = coordinator.start(.backupWallet(address: walletAddress))
         guard proceed else {
             finish()
             return
