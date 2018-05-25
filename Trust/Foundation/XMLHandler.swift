bf7853be25443d3babab24577a079ef82b0c9a65
James Sangalli <j.l.sangalli@gmail.com>
2018-05-25 22:05:08 +0800

    handles BinaryTime, IA5String and Integer, part way through enumeration

--- Trust/Foundation/XMLHandler.swift
+++ Trust/Foundation/XMLHandler.swift
@@ -25,14 +25,104 @@ extension XML.Accessor {
             } else {
                 return nil
             }
-            case .sequence(let elements):
-                return elements.first {
-                    $0.attributes[attributeName] == attributeValue
-                }
-            case .failure:
-                return nil
+        case .sequence(let elements):
+            return elements.first {
+                $0.attributes[attributeName] == attributeValue
+            }
+        case .failure:
+            return nil
         }
     }
+}
+
+enum FieldType {
+    init(field: XML.Element) {
+        self = {
+            if let type = field.attributes["type"] {
+                switch type {
+                case "IA5String":
+                    return .IA5String(field: field)
+                case "BinaryTime":
+                    return .BinaryTime(field: field)
+                case "Integer":
+                    return .Integer(field: field)
+                case "Enumeration":
+                    return .Enumeration(field: field)
+                default:
+                    return .IA5String(field: field)
+                }
+            } else {
+                return .IA5String(field: field)
+            }
+        }()
+    }
+    
+    func parseValueAsAscii(tokenValueHex: String) -> String {
+        let (start, end) = getIndices()
+        let value = tokenValueHex.substring(with: Range(uncheckedBounds: (start, end))).hexa2Bytes
+        return String(data: Data(bytes: value), encoding: .utf8) ?? "TBD"
+    }
+    
+    func parseValueAsInt(tokenValueHex: String) -> Int {
+        let (start, end) = getIndices()
+        let value = tokenValueHex.substring(with: Range(uncheckedBounds: (start, end))).hexa2Bytes
+        if let intValue = Int(MarketQueueHandler.bytesToHexa(value), radix: 16) {
+            return intValue
+        }
+        return 1
+    }
+    
+    func parseValueFromEnumeration(tokenValueHex: String, id: String) -> String {
+        let (start, end) = getIndices()
+        let value = tokenValueHex.substring(with: Range(uncheckedBounds: (start, end))).hexa2Bytes
+        if let entityKey = Int(MarketQueueHandler.bytesToHexa(value), radix: 16) {
+            //get entity key and id for corresponding value of enumeration
+            
+        }
+        return ""
+    }
+    
+    func handleBitmaskIndices(bitmask: String) -> (Int, Int) {
+        var startingNumber = 0
+        var endingNumber = 0
+        //todo temporary
+        let trimmedBitmask = bitmask.substring(from: 32)
+        for i in 0...trimmedBitmask.count {
+            if trimmedBitmask.substring(with: Range(uncheckedBounds: (i, i + 1))) == "F" {
+                startingNumber = i
+                break
+            }
+        }
+        let strippedBitmask = trimmedBitmask.replacingOccurrences(of: "0", with: "")
+        endingNumber = strippedBitmask.count + startingNumber
+        return (startingNumber, endingNumber)
+    }
+    
+    func getIndices() -> (Int, Int) {
+        switch self {
+        case .IA5String(let field):
+            if let bitmask = field.attributes["bitmask"] {
+                return handleBitmaskIndices(bitmask: bitmask)
+            }
+        case .BinaryTime(let field):
+            if let bitmask = field.attributes["bitmask"] {
+                return handleBitmaskIndices(bitmask: bitmask)
+            }
+        case .Integer(let field):
+            if let bitmask = field.attributes["bitmask"] {
+                return handleBitmaskIndices(bitmask: bitmask)
+            }
+        case .Enumeration(let field):
+            if let bitmask = field.attributes["bitmask"] {
+                return handleBitmaskIndices(bitmask: bitmask)
+            }
+        }
+        return (0, 0)
+    }
+    case Enumeration(field: XML.Element)
+    case IA5String(field: XML.Element)
+    case BinaryTime(field: XML.Element)
+    case Integer(field: XML.Element)
 }
 
 public class XMLHandler {
@@ -60,14 +150,15 @@ public class XMLHandler {
         let tokenHex = MarketQueueHandler.bytesToHexa(tokenBytes32.serialize().bytes)
         let location = getLocality(attribute: tokenHex.substring(to: 2), lang: lang)
         let venue = getVenue(attribute: tokenHex.substring(with: Range(uncheckedBounds: (2, 4))), lang: lang)
-        let time = Int(tokenHex.substring(with: Range(uncheckedBounds: (4, 12))), radix: 16)
-                ?? Int(Date.tomorrow.timeIntervalSince1970.rounded(to: 0))
+        let timeField = xml["asset"]["fields"]["field"].getElement(attributeName: "id", attributeValue: "time")!
+        let time = FieldType(field: timeField).parseValueAsInt(tokenValueHex: tokenHex)
         //translatable to ascii
-        let countryA = tokenHex.substring(with: Range(uncheckedBounds: (12, 18))).hexa2Bytes
-        let countryB = tokenHex.substring(with: Range(uncheckedBounds: (18, 24))).hexa2Bytes
-        let countryAString = String(data: Data(bytes: countryA), encoding: .utf8) ?? "TBD"
-        let countryBString = String(data: Data(bytes: countryB), encoding: .utf8) ?? "TBD"
-        let match = Int(tokenHex.substring(with: Range(uncheckedBounds: (24, 26))), radix: 16) ?? 0
+        let countryAField = xml["asset"]["fields"]["field"].getElement(attributeName: "id", attributeValue: "countryA")!
+        let countryAString = FieldType(field: countryAField).parseValueAsAscii(tokenValueHex: tokenHex)
+        let countryBField = xml["asset"]["fields"]["field"].getElement(attributeName: "id", attributeValue: "countryB")!
+        let countryBString = FieldType(field: countryBField).parseValueAsAscii(tokenValueHex: tokenHex)
+        let matchField = xml["asset"]["fields"]["field"].getElement(attributeName: "id", attributeValue: "match")!
+        let match = FieldType(field: matchField).parseValueAsInt(tokenValueHex: tokenHex)
         let category = Int(tokenHex.substring(with: Range(uncheckedBounds: (26, 28))), radix: 16) ?? 0
         let number = Int(tokenHex.substring(with: Range(uncheckedBounds: (28, 32))), radix: 16) ?? 0
         //TODO derive/extract from XML
