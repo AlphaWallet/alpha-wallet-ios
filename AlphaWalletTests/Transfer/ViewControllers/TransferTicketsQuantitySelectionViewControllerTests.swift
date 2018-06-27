0d4b508cd6b95de82727645143591227524e59a9
Hwee-Boon Yar <hboon@motionobj.com>
2018-06-27 19:06:45 +0800

    Fix: update and re-enable sole snapshot test after upgrading Xcode to 9.3 for tests

--- AlphaWalletTests/Transfer/ViewControllers/TransferTicketsQuantitySelectionViewControllerTests.swift
+++ AlphaWalletTests/Transfer/ViewControllers/TransferTicketsQuantitySelectionViewControllerTests.swift
@@ -11,15 +11,15 @@ class TransferTicketsQuantitySelectionViewControllerTests: FBSnapshotTestCase  {
         recordMode = false
     }
 
-//    func testTransferTicketQuantitySelectionViewControllerCanBeCreated() {
-//        let token = TokenObject()
-//        let type = PaymentFlow.send(type: .stormBird(token))
-//        let ticket = Ticket(id: "1", index: 1, city: "", name: "", venue: "", match: 9, date: GeneralisedTime(string: "20010203160500+0300")!, seatId: 1, category: "MATCH CLUB", countryA: "Team A", countryB: "Team B")
-//        let ticketHolder = TicketHolder(tickets: [ticket], status: .available, contractAddress: "0x1")
-//        let controller = TransferTicketsQuantitySelectionViewController(paymentFlow: type)
-//        let viewModel = TransferTicketsQuantitySelectionViewModel(token: TokenObject(), ticketHolder: ticketHolder)
-//        controller.configure(viewModel: viewModel)
-//
-//        FBSnapshotVerifyView(controller.view)
-//    }
+    func testTransferTicketQuantitySelectionViewControllerCanBeCreated() {
+        let token = TokenObject()
+        let type = PaymentFlow.send(type: .stormBird(token))
+        let ticket = Ticket(id: "1", index: 1, city: "", name: "", venue: "", match: 9, date: GeneralisedTime(string: "20010203160500+0300")!, seatId: 1, category: "MATCH CLUB", countryA: "Team A", countryB: "Team B")
+        let ticketHolder = TicketHolder(tickets: [ticket], status: .available, contractAddress: "0x1")
+        let controller = TransferTicketsQuantitySelectionViewController(paymentFlow: type)
+        let viewModel = TransferTicketsQuantitySelectionViewModel(token: TokenObject(), ticketHolder: ticketHolder)
+        controller.configure(viewModel: viewModel)
+
+        FBSnapshotVerifyView(controller.view)
+    }
 }
