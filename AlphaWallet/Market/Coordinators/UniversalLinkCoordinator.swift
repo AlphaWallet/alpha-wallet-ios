3036c40c18de9850ccdca6ac65c48e973421ece0
James Sangalli <j.l.sangalli@gmail.com>
2018-06-27 23:13:25 +1000

    almost working with web3swift ecrecover

--- AlphaWallet/Market/Coordinators/UniversalLinkCoordinator.swift
+++ AlphaWallet/Market/Coordinators/UniversalLinkCoordinator.swift
@@ -5,7 +5,7 @@ import Alamofire
 import BigInt
 import Realm
 import TrustKeystore
-//import web3swift
+import web3swift
 
 protocol UniversalLinkCoordinatorDelegate: class {
 	func viewControllerForPresenting(in coordinator: UniversalLinkCoordinator) -> UIViewController?
@@ -114,8 +114,7 @@ class UniversalLinkCoordinator: Coordinator {
         return true
     }
 
-	//Returns true if handled
-
+    //Returns true if handled
     func handleUniversalLink(url: URL) -> Bool {
         let prefix = UniversalLinkHandler().urlPrefix
         let matchedPrefix = url.description.hasPrefix(prefix)
@@ -132,27 +131,57 @@ class UniversalLinkCoordinator: Coordinator {
         let isStormBirdContract = xmlAddress.eip55String.sameContract(as: signedOrder.order.contractAddress)
         importTicketViewController?.url = url
         importTicketViewController?.contract = signedOrder.order.contractAddress
-        //let recoveredSigner = web3.personal.ecrecover(personalMessage: Data(bytes: signedOrder.message), signature: signature)
-        getTicketDetailsAndEcRecover(signedOrder: signedOrder) { result in
-            if let goodResult = result {
-                //user can pay gas for free import links if they are not covered by our server
-                if signedOrder.order.price > 0 || !isStormBirdContract {
-                    if let balance = self.ethBalance {
-                        balance.subscribeOnce { value in
-                            if value > signedOrder.order.price {
-                                let _ = self.handlePaidUniversalLink(signedOrder: signedOrder, ticketHolder: goodResult)
-                            } else {
-                                if let price = self.ethPrice {
-                                    if price.value == nil {
-                                        let ethCost = self.convert(ethCost: signedOrder.order.price)
-                                        self.showImportError(errorMessage: R.string.localizable.aClaimTicketFailedNotEnoughEthTitle(), ticketHolder: goodResult, ethCost: ethCost.description)
-                                    }
-                                    price.subscribe { [weak self] value in
-                                        if let celf = self {
-                                            if let price = price.value {
-                                                let (ethCost, dollarCost) = celf.convert(ethCost: signedOrder.order.price, rate: price)
-                                                celf.showImportError(errorMessage: R.string.localizable.aClaimTicketFailedNotEnoughEthTitle(), ticketHolder: goodResult, ethCost: ethCost.description, dollarCost: dollarCost.description)
-                                            }
+        //need to hash message here because the web3swift implementation adds prefix
+        let messageHash = Data(bytes: signedOrder.message).sha3(.keccak256)
+        //note: web3swift takes the v value as v - 27, so we need to manually convert this
+        let vValue = signedOrder.signature.drop0x.substring(from: 128)
+        let vInt = Int(vValue, radix: 16)! - 27
+        let vString = "0" + String(vInt)
+        let signature = "0x" + signedOrder.signature.drop0x.substring(from: 128) + vString
+        let nodeURL = Config().rpcURL
+        let recoveredSigner = web3(provider: Web3HttpProvider(nodeURL)!).personal.ecrecover(
+            hash: messageHash,
+            signature: Data(bytes: signature.hexa2Bytes)
+        )
+        let recoverAddress = Address(string: try! recoveredSigner.dematerialize().address)!
+        let contractAsAddress = Address(string: signedOrder.order.contractAddress)!
+        //gather signer address balance
+        GetStormBirdBalanceCoordinator(web3: Web3Swift()).getStormBirdBalance(for: recoverAddress, contract: contractAsAddress) { result in
+            //filter null tickets
+            let filteredTokens = self.checkERC875TokensAreAvailable(
+                    indices: signedOrder.order.indices,
+                    balance: try! result.dematerialize()
+            )
+            if filteredTokens.isEmpty {
+                self.showImportError(errorMessage: R.string.localizable.aClaimTicketInvalidLinkTryAgain())
+            }
+
+            let ticketHolder = self.sortTickets(
+                    filteredTokens,
+                    signedOrder.order.indices,
+                    signedOrder.order.contractAddress
+            )
+
+            if signedOrder.order.price > 0 || !isStormBirdContract {
+                if let balance = self.ethBalance {
+                    balance.subscribeOnce { value in
+                        if value > signedOrder.order.price {
+                            let _ = self.handlePaidUniversalLink(signedOrder: signedOrder, ticketHolder: ticketHolder)
+                        } else {
+                            if let price = self.ethPrice {
+                                if price.value == nil {
+                                    let ethCost = self.convert(ethCost: signedOrder.order.price)
+                                    self.showImportError(
+                                            errorMessage: R.string.localizable.aClaimTicketFailedNotEnoughEthTitle(),
+                                            ticketHolder: ticketHolder,
+                                            ethCost: ethCost.description
+                                    )
+                                }
+                                price.subscribe { [weak self] value in
+                                    if let celf = self {
+                                        if let price = price.value {
+                                            let (ethCost, dollarCost) = celf.convert(ethCost: signedOrder.order.price, rate: price)
+                                            celf.showImportError(errorMessage: R.string.localizable.aClaimTicketFailedNotEnoughEthTitle(), ticketHolder: ticketHolder, ethCost: ethCost.description, dollarCost: dollarCost.description)
                                         }
                                     }
                                 }
@@ -160,79 +189,42 @@ class UniversalLinkCoordinator: Coordinator {
                         }
                     }
                 }
-                else {
-                    let _ = self.usePaymentServerForFreeTransferLinks(
-                            signedOrder: signedOrder,
-                            ticketHolder: goodResult
-                    )
-                }
-            } else {
-                self.showImportError(errorMessage: R.string.localizable.aClaimTicketInvalidLinkTryAgain())
+            }
+            else {
+                //free transfer
+                let _ = self.usePaymentServerForFreeTransferLinks(
+                        signedOrder: signedOrder,
+                        ticketHolder: ticketHolder
+                )
             }
 
         }
         return true
-	}
-
-    func importPaidSignedOrder(signedOrder: SignedOrder, tokenObject: TokenObject) {
-        updateImportTicketController(with: .processing)
-        delegate?.importPaidSignedOrder(signedOrder: signedOrder, tokenObject: tokenObject) { successful in
-            if self.importTicketViewController != nil {
-                if let vc = self.importTicketViewController, var _ = vc.viewModel {
-                    if successful {
-                        self.showImportSuccessful()
-                    } else {
-                        //TODO Pass in error message
-                        self.showImportError(errorMessage: R.string.localizable.aClaimTicketFailedTitle())
-                    }
-                }
-            }
-        }
-
     }
 
     private func stringEncodeIndices(_ indices: [UInt16]) -> String {
         return indices.map(String.init).joined(separator: ",")
     }
-
-    private func getTicketDetailsAndEcRecover(
-            signedOrder: SignedOrder,
-            completion: @escaping( _ response: TicketHolder?) -> Void
-    ) {
-        let indices = signedOrder.order.indices
-        let parameters = createHTTPParametersForPaymentServer(
-            signedOrder: signedOrder,
-            isForTransfer: false
-        )
-        
-        Alamofire.request(Constants.getTicketInfoFromServer, method: .get, parameters: parameters).responseJSON { response in
-            if let data = response.data, let utf8Text = String(data: data, encoding: .utf8) {
-                if let statusCode = response.response?.statusCode {
-                    if statusCode > 299 {
-                        completion(nil)
-                        return
-                    }
-                }
-                var array = utf8Text.split(separator: ",").map(String.init)
-                if array.isEmpty || array[0] == "invalid indices" {
-                    completion(nil)
-                    return
-                }
-                //start at one to slice off address
-                let bytes32Tickets = Array(array[1...])
-                completion(
-                    self.sortTickets(
-                        bytes32Tickets,
-                        indices,
-                        signedOrder.order.contractAddress
-                    )
-                )
-            } else {
-                completion(nil)
-            }
-        }
-    }
     
+    func checkERC875TokensAreAvailable(indices: [UInt16], balance: [String]) -> [String] {
+        var filteredTokens = [String]()
+        if balance.count < indices.count {
+            return [String]()
+        }
+        for i in 0..<balance.count {
+            let token: String = balance[Int(indices[i])]
+            //all of the indices provided should map to a valid non null ticket
+            if let tokenValue = BigUInt(token, radix: 16) {
+                if tokenValue == 0 {
+                    //if null ticket at any index then the deal cannot happen
+                    return [String]()
+                }
+            }
+            filteredTokens.append(token)
+        }
+        return filteredTokens
+    }
+
     private func sortTickets(_ bytes32Tickets: [String], _ indices: [UInt16], _ contractAddress: String) -> TicketHolder {
         var tickets = [Ticket]()
         let xmlHandler = XMLHandler()
@@ -300,6 +292,23 @@ class UniversalLinkCoordinator: Coordinator {
     private func showImportError(errorMessage: String, ticketHolder: TicketHolder? = nil, ethCost: String? = nil, dollarCost: String? = nil) {
         updateImportTicketController(with: .failed(errorMessage: errorMessage), ticketHolder: ticketHolder, ethCost: ethCost, dollarCost: dollarCost)
 	}
+    
+    func importPaidSignedOrder(signedOrder: SignedOrder, tokenObject: TokenObject) {
+        updateImportTicketController(with: .processing)
+        delegate?.importPaidSignedOrder(signedOrder: signedOrder, tokenObject: tokenObject) { successful in
+            if self.importTicketViewController != nil {
+                if let vc = self.importTicketViewController, var _ = vc.viewModel {
+                    if successful {
+                        self.showImportSuccessful()
+                    } else {
+                        //TODO Pass in error message
+                        self.showImportError(errorMessage: R.string.localizable.aClaimTicketFailedTitle())
+                    }
+                }
+            }
+        }
+        
+    }
 
     //handling free transfers, sell links cannot be handled here
 	private func importUniversalLink(query: String, parameters: Parameters) {
