5f9099df4f2750746bb732015a62e8cae0ee956b
Hwee-Boon Yar <hboon@motionobj.com>
2018-06-30 16:37:33 +0800

    Suggested (but commented out) fix for balance coordinator not working while ecrecover

--- AlphaWallet/Tokens/Coordinators/GetStormBirdBalanceCoordinator.swift
+++ AlphaWallet/Tokens/Coordinators/GetStormBirdBalanceCoordinator.swift
@@ -11,6 +11,14 @@ import JavaScriptKit
 class GetStormBirdBalanceCoordinator {
 
     private let web3: Web3Swift
+    //TODO remove
+    var tag = ""
+    //TODO remove
+    deinit {
+        if tag == "foo" {
+            NSLog("xxx deinit balance coordinator")
+        }
+    }
 
     init(
         web3: Web3Swift
@@ -24,12 +32,19 @@ class GetStormBirdBalanceCoordinator {
         completion: @escaping (Result<[String], AnyError>) -> Void
     ) {
         let request = GetStormBirdBalanceEncode(address: address)
+        //TODO remove
+        let tag2 = tag
         web3.request(request: request) { result in
             switch result {
             case .success(let res):
                 let request2 = EtherServiceRequest(
                     batch: BatchFactory().create(CallRequest(to: contract.description, data: res))
                 )
+                //TODO remove
+                if tag2 == "foo" {
+                    NSLog("xxx back in balance coordinator")
+                }
+                //TODO immediately after this next Session.send() call, this instance of GetStormBirdBalanceEncode will be destroyed if we don't store a strong reference to it because the block passed on Session.send() does not hold a strong reference to self (because of the weak self)
                 Session.send(request2) { [weak self] result2 in
                     switch result2 {
                     case .success(let balance):
