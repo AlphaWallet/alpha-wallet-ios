79f10ea32ace9524eebd9b3b09a6a5b2b3370a3a
James Sangalli <j.l.sangalli@gmail.com>
2018-07-15 20:42:12 +0800

    check if ERC721 with call to ownerOf

--- AlphaWallet/Tokens/Coordinators/GetIsERC721ContractCoordinator.swift
+++ AlphaWallet/Tokens/Coordinators/GetIsERC721ContractCoordinator.swift
@@ -25,30 +25,8 @@ class GetIsERC721ContractCoordinator {
         let request = GetIsERC721Encode()
         web3.request(request: request) { result in
             switch result {
-            case .success(let res):
-                let request2 = EtherServiceRequest(
-                        batch: BatchFactory().create(CallRequest(to: contract.description, data: res))
-                )
-                Session.send(request2) { [weak self] result2 in
-                    switch result2 {
-                    case .success(let address):
-                        let request = GetIsERC721Decode(data: address)
-                        self?.web3.request(request: request) { result in
-                            switch result {
-                            //if a successful return then it must be ERC721, else it is not
-                            case .success(let res):
-                                NSLog("getIsERC721 result \(res) ")
-                                completion(.success(true))
-                            case .failure(let error):
-                                NSLog("getIsERC721 3 error \(error)")
-                                completion(.failure(AnyError(error)))
-                            }
-                        }
-                    case .failure(let error):
-                        NSLog("getIsERC721 2 error \(error)")
-                        completion(.failure(AnyError(error)))
-                    }
-                }
+            case .success:
+                completion(.success(true))
             case .failure(let error):
                 NSLog("getIsERC721 error \(error)")
                 completion(.failure(AnyError(error)))
