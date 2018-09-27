e3209c58a5fed0070b941d2caf55aefb3a6bdb9e
James Sangalli <j.l.sangalli@gmail.com>
2018-09-27 21:11:24 +0800

    verified button opens on other networks

--- AlphaWallet/Settings/Types/Constants.swift
+++ AlphaWallet/Settings/Types/Constants.swift
@@ -45,12 +45,14 @@ public struct Constants {
     public static let mainnetEtherscanAPI = "https://api.etherscan.io/api?module=account&action=txlist&address="
     public static let ropstenEtherscanAPI = "https://ropsten.etherscan.io/api?module=account&action=txlist&address="
     public static let rinkebyEtherscanAPI = "https://rinkeby.etherscan.io/api?module=account&action=txlist&address="
+    public static let kovanEtherscanAPI = "https://rinkeby.etherscan.io/api?module=account&action=txlist&address="
 
     //etherscan contract details web page
     public static let mainnetEtherscanContractDetailsWebPageURL = "https://etherscan.io/address/"
     public static let ropstenEtherscanContractDetailsWebPageURL = "https://ropsten.etherscan.io/address/"
     public static let rinkebyEtherscanContractDetailsWebPageURL = "https://rinkeby.etherscan.io/address/"
-    
+    public static let kovanEtherscanContractDetailsWebPageURL = "https://kovan.etherscan.io/address/"
+
     //ERC specs
     public static let erc721InterfaceHash = "0x9a20483d"
     
