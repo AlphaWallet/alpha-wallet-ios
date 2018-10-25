bee3b202daad7e558a7383c36c34a5ebef3e3b30
Hwee-Boon Yar <hboon@motionobj.com>
2018-10-25 17:21:46 +0800

    Custom UI for prompt to sign message

---
+++ AlphaWallet/Transfer/ViewModels/ConfirmSignMessageTableViewCellViewModel.swift
@@ -0,0 +1,13 @@
+// Copyright © 2018 Stormbird PTE. LTD.
+
+import Foundation
+import UIKit
+
+struct ConfirmSignMessageTableViewCellViewModel {
+    let backgroundColor: UIColor
+    let nameTextFont: UIFont
+    let valueTextFont: UIFont
+    let valueTextColor: UIColor
+    let name: String
+    let value: String
+}
---
+++ AlphaWallet/Transfer/Views/ConfirmSignMessageTableViewCell.swift
@@ -0,0 +1,30 @@
+// Copyright © 2018 Stormbird PTE. LTD.
+
+import Foundation
+import UIKit
+
+class ConfirmSignMessageTableViewCell: UITableViewCell {
+    static let identifier = "ConfirmSignMessageTableViewCell"
+
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func configure(viewModel: ConfirmSignMessageTableViewCellViewModel) {
+        contentView.backgroundColor = viewModel.backgroundColor
+        selectionStyle = .none
+
+        textLabel?.font = viewModel.nameTextFont
+
+        detailTextLabel?.numberOfLines = 0
+        detailTextLabel?.font = viewModel.valueTextFont
+        detailTextLabel?.textColor = viewModel.valueTextColor
+
+        textLabel?.text = viewModel.name
+        detailTextLabel?.text = viewModel.value
+    }
+}
