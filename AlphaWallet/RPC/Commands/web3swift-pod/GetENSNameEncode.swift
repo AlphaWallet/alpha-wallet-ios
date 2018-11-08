a3d2062d77e2ed0972ae0be2ca15223ed60afbee
James Sangalli <j.l.sangalli@gmail.com>
2018-11-08 15:50:02 +0800

    add methods for getting ENS info from node

---
+++ AlphaWallet/RPC/Commands/web3swift-pod/GetENSNameEncode.swift
@@ -0,0 +1,10 @@
+//
+// Created by James Sangalli on 8/11/18.
+//
+
+import Foundation
+
+struct GetENSOwnerEncode {
+    let abi = "{\"constant\":true,\"inputs\":[{\"name\":\"node\",\"type\":\"bytes32\"}],\"name\":\"owner\",\"outputs\":[{\"name\":\"\",\"type\":\"address\"}]"
+    let name = "owner"
+}
--- AlphaWallet/Settings/Types/Constants.swift
+++ AlphaWallet/Settings/Types/Constants.swift
@@ -2,6 +2,7 @@
 
 import Foundation
 import BigInt
+import web3swift
 
 public struct Constants {
     public static let keychainKeyPrefix = "alphawallet"
@@ -20,7 +21,6 @@ public struct Constants {
     // fee master
     public static let paymentServer = "https://app.awallet.io:80/api/claimToken"
     public static let paymentServerSupportsContractEndPoint = "https://app.awallet.io:80/api/checkContractIsSupportedForFreeTransfers"
-    public static let getTicketInfoFromServer = "https://app.awallet.io:80/api/ecrecoverAndGetTickets"
 
     // social
     public static let website = "https://community.awallet.io/c/alphawallet-app/ios-feedback"
@@ -55,11 +55,16 @@ public struct Constants {
     //OpenSea links for erc721 assets
     public static let openseaAPI = "https://api.opensea.io/"
     public static let openseaAPIKEY = "11ba1b4f0c4246aeb07b1f8e5a20525f" // X-API-KEY
-
     public static let cryptoKittiesContractAddress = "0x06012c8cf97bead5deae237070f9587f8e7a266d"
 
-    public static let etherReceivedNotificationIdentifier = "etherReceivedNotificationIdentifier"
+    //ENS
+    public static let ENSRegistrarAddress = EthereumAddress("0x314159265dD8dbb310642f98f50C066173C1259b")!
+    public static let ENSRegistrarRopsten = ""
+    public static let ENSRegistrarRinkeby = ""
 
+
+    //Misc
+    public static let etherReceivedNotificationIdentifier = "etherReceivedNotificationIdentifier"
     public static let legacy875Addresses = ["830e1650a87a754e37ca7ed76b700395a7c61614", "a66a3f08068174e8f005112a8b2c7a507a822335"]
 }
 
