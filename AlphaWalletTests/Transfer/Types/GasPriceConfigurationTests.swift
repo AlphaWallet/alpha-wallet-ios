42bb1be61adc5a6cb040c166e6c8f9af0790ce9a
James Sangalli <j.l.sangalli@gmail.com>
2018-12-01 03:05:26 +0800

    adapt gas tests

--- AlphaWalletTests/Transfer/Types/GasPriceConfigurationTests.swift
+++ AlphaWalletTests/Transfer/Types/GasPriceConfigurationTests.swift
@@ -7,8 +7,8 @@ import BigInt
 class GasPriceConfigurationTests: XCTestCase {
     
     func testDefault() {
-        XCTAssertEqual(BigInt(90000), GasPriceConfiguration.defaultPrice)
-        XCTAssertEqual(BigInt(21000), GasPriceConfiguration.minPrice)
-        XCTAssertEqual(BigInt(4712388), GasPriceConfiguration.maxPrice)
+        XCTAssertEqual(BigInt(9000000000), GasPriceConfiguration.defaultPrice)
+        XCTAssertEqual(BigInt(1000000000), GasPriceConfiguration.minPrice)
+        XCTAssertEqual(BigInt(100000000000), GasPriceConfiguration.maxPrice)
     }
 }
