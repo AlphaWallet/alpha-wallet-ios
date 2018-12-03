de469a7c94168d78ecae60c7c763e03f38c45228
James Sangalli <j.l.sangalli@gmail.com>
2018-12-03 14:22:01 +0800

    correct abi for 721 transfers

--- AlphaWallet/RPC/Commands/web3wift-web3js/ContractERC721Transfer.swift
+++ AlphaWallet/RPC/Commands/web3wift-web3js/ContractERC721Transfer.swift
@@ -15,8 +15,8 @@ struct ContractERC721Transfer: Web3Request {
     let contractAddress: String
 
     var type: Web3RequestType {
-        let abiLegacyTransfer = "{\"constant\":false,\"inputs\":[{\"name\":\"_to\",\"type\":\"address\"},{\"name\":\"_tokenId\",\"type\":\"uint256\"}],\"name\":\"transfer\",\"outputs\":[],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"function\"}, [\"\(to)\" \(tokenId)]"
-        let abiSafeTransferFrom = "{ \"constant\": false, \"inputs\": [ { \"name\": \"_from\", \"type\": \"address\" }, { \"name\": \"_to\", \"type\": \"address\" }, { \"name\": \"_tokenId\", \"type\": \"uint256\" } ], \"name\": \"safeTransferFrom\", \"outputs\": [], \"payable\": true, \"stateMutability\": \"payable\", \"type\": \"function\"},  [\"\(from)\" \(to), \(tokenId)]"
+        let abiLegacyTransfer = "{\"constant\":false,\"inputs\":[{\"name\":\"_to\",\"type\":\"address\"},{\"name\":\"_tokenId\",\"type\":\"uint256\"}],\"name\":\"transfer\",\"outputs\":[],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"function\"}, [\"\(to)\", \(tokenId)]"
+        let abiSafeTransferFrom = "{ \"constant\": false, \"inputs\": [ { \"name\": \"_from\", \"type\": \"address\" }, { \"name\": \"_to\", \"type\": \"address\" }, { \"name\": \"_tokenId\", \"type\": \"uint256\" } ], \"name\": \"safeTransferFrom\", \"outputs\": [], \"payable\": true, \"stateMutability\": \"payable\", \"type\": \"function\"},  [\"\(from)\", \(to), \(tokenId)]"
         let abi: String
         let isLegacy = Constants.legacy721Addresses.contains { $0.sameContract(as: contractAddress) }
         if isLegacy {
