7302a63999913dadf083f5dddd26af8cfd6c8982
James Sangalli <j.l.sangalli@gmail.com>
2019-07-18 16:08:33 +1000

    get CN from result

--- AlphaWallet/TokenScriptClient/Models/TokenScriptSignatureVerifier.swift
+++ AlphaWallet/TokenScriptClient/Models/TokenScriptSignatureVerifier.swift
@@ -3,18 +3,18 @@
 import Foundation
 import PromiseKit
 import Alamofire
+import SwiftyJSON
 
 class TokenScriptSignatureVerifier {
 
     func verify(xml: String, isOfficial: Bool) -> Promise<TokenScriptSignatureVerificationType> {
         return Promise { seal in
             if isOfficial {
-                verifyXMLSignatureViaAPI(xml: xml) { passed in
-                    if passed {
-                        //TODO get domain from validator
-                        seal.fulfill(.verified(domainName: "alphawallet.com"))
-                    } else {
+                verifyXMLSignatureViaAPI(xml: xml) { result in
+                    if result == "failed" {
                         seal.fulfill(.verificationFailed)
+                    } else {
+                        seal.fulfill(.verified(domainName: result))
                     }
                 }
             } else {
@@ -23,9 +23,9 @@ class TokenScriptSignatureVerifier {
         }
     }
 
-    func verifyXMLSignatureViaAPI(xml: String, completion: @escaping (Bool) -> Void) {
+    func verifyXMLSignatureViaAPI(xml: String, completion: @escaping (String) -> Void) {
         guard let xmlAsData = xml.data(using: String.Encoding.utf8) else {
-            completion(false)
+            completion("failed")
             return
         }
         let url = URL(string: Constants.tokenScriptValidatorAPI)!
@@ -46,15 +46,25 @@ class TokenScriptSignatureVerifier {
             case .success(let upload, _, _):
                 upload.validate()
                 upload.responseJSON { response in
-                    guard response.result.isSuccess, let unwrappedResponse = response.response, unwrappedResponse.statusCode <= 299 else {
-                        completion(false)
+                    guard response.result.isSuccess,
+                          let unwrappedResponse = response.response,
+                          unwrappedResponse.statusCode <= 299,
+                          let value = response.result.value
+                    else {
+                        completion("failed")
                         return
                     }
-                    completion(true)
+                    let json = JSON(value)
+                    guard let CN = json["subject"].string else { 
+                        //Should never hit
+                        completion("unknown CN")
+                        return
+                    }
+                    let domain = CN.replacingOccurrences(of: "CN=", with: "")
+                    completion(domain)
                 }
-            case .failure(let encodingError):
-                NSLog("xxx error: \(encodingError)")
-                completion(false)
+            case .failure(let _):
+                completion("failed")
             }
         })
     }
