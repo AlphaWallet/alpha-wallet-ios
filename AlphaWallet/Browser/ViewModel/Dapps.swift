c9799c12ee11cb4f9ca585114622e1fa8c37064b
James Sangalli <j.l.sangalli@gmail.com>
2019-07-31 16:23:04 +1000

    Fix find the rabbit url

--- AlphaWallet/Browser/ViewModel/Dapps.swift
+++ AlphaWallet/Browser/ViewModel/Dapps.swift
@@ -7,7 +7,7 @@ enum Dapps {
         Dapp(name: "State of the ÐApps", description: "Directory of Decentralized Applications", url: "https://www.stateofthedapps.com/", cat: "Directory"),
         Dapp(name: "BulkSender", description: "Batch sending of tokens", url: "https://bulksender.app/", cat: "Finance"),
         Dapp(name: "ERC20 Faucet", description: "Receive free erc20 token for testing", url: "https://erc20faucet.com/", cat: "Tool"),
-        Dapp(name: "Find The Rabbit", description: "Provable shell game", url: "https://multisender.app/", cat: "Game"),
+        Dapp(name: "Find The Rabbit", description: "Provable shell game", url: "https://findtherabbit.me/", cat: "Game"),
         Dapp(name: "Axie Infinity", description: "Collect and raise fantasy creatures", url: "https://axieinfinity.com/", cat: "Games"),
         Dapp(name: "ChickenHunt", description: "character-growing IDLE game", url: "https://chickenhunt.io/", cat: "Games"),
         Dapp(name: "CryptoCare", description: "Social Impact Collectibles", url: "https://cryptocare.tech/adopt/cryptocare", cat: "Games"),
