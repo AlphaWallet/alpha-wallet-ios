15b254db73e0cc1181a03b6d50401be02347687e
Hwee-Boon Yar <hboon@motionobj.com>
2019-08-11 23:33:57 +0800

    Improve wallet tab UI efficiency by rate limiting refreshes. Otherwise refresh (eg. from pull down or periodic refreshes) can trigger N UI refreshes for N networks

---
+++ AlphaWallet/Core/Types/RateLimiter.swift
@@ -0,0 +1,24 @@
+//Copyright © 2019 Stormbird PTE. LTD.
+
+import Foundation
+
+///One limitation of this class due to simplification: if "requests" keep coming in, each with the time limit from the last, the block will not fire until one of the request gets a breather of `limit`
+class RateLimiter {
+    private let block: () -> ()
+    private let limit: TimeInterval
+    private var timer: Timer?
+
+    init(limit: TimeInterval, block: @escaping () -> ()) {
+        self.limit = limit
+        self.block = block
+    }
+
+    func run() {
+        timer?.invalidate()
+        timer = Timer.scheduledTimer(timeInterval: limit, target: self, selector: #selector(runBlock), userInfo: nil, repeats: false)
+    }
+
+    @objc private func runBlock() {
+        block()
+    }
+}
