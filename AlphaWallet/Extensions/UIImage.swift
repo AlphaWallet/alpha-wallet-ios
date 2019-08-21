8fd80cfbbf5abd74db2026f44a9171d5cb8fe29d
Hwee-Boon Yar <hboon@motionobj.com>
2019-08-21 16:55:53 +0800

    Remove duplicate function

--- AlphaWallet/Extensions/UIImage.swift
+++ AlphaWallet/Extensions/UIImage.swift
@@ -4,20 +4,6 @@ import Foundation
 import UIKit
 
 extension UIImage {
-    static func filled(with color: UIColor) -> UIImage {
-        let pixelScale = UIScreen.main.scale
-        let pixelSize = 1 / pixelScale
-        let fillSize = CGSize(width: pixelSize, height: pixelSize)
-        let fillRect = CGRect(origin: CGPoint.zero, size: fillSize)
-        UIGraphicsBeginImageContextWithOptions(fillRect.size, false, pixelScale)
-        let graphicsContext = UIGraphicsGetCurrentContext()
-        graphicsContext!.setFillColor(color.cgColor)
-        graphicsContext!.fill(fillRect)
-        let image = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        return image!
-    }
-
     public convenience init?(color: UIColor, size: CGSize = CGSize(width: 1, height: 1)) {
         let rect = CGRect(origin: .zero, size: size)
         UIGraphicsBeginImageContextWithOptions(rect.size, false, UIScreen.main.scale)
