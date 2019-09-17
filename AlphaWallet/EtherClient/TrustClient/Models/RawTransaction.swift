9e8118cd122a3a372fb7e13fa09b5d7e31e7610d
Hwee-Boon Yar <hboon@motionobj.com>
2019-09-17 11:54:12 +0800

    Fix: crash when etherscan API returns ERC20 transfer transaction data which has an empty `to` field despite the transaction actually having that data (i.e that transaction record returned by the etherscan API is malformed)

--- AlphaWallet/EtherClient/TrustClient/Models/RawTransaction.swift
+++ AlphaWallet/EtherClient/TrustClient/Models/RawTransaction.swift
@@ -18,9 +18,15 @@ struct RawTransaction: Decodable {
     let gasUsed: String
     let error: String?
 
-    var toAddress: AlphaWallet.Address {
+    ///
+    ///It is possible for the etherscan.io API to return an empty `to` even if the transaction actually has a `to`. It doesn't seem to be linked to `"isError" = "1"`, because other transactions that fail (with isError="1") has a non-empty `to`.
+    ///
+    ///Eg. transaction with an empty `to` in API despite `to` is shown as non-empty in the etherscan.io web page:https: //ropsten.etherscan.io/tx/0x0c87d2acb0ecaf1221e599ad4f65edf77c97956d6534feb0afa68ee5c41c4e28
+    ///
+    ///So it must be a optional
+    var toAddress: AlphaWallet.Address? {
         //TODO We use the unchecked version because it was easier to provide an Address instance this way. Good to remove it
-        return AlphaWallet.Address(uncheckedAgainstNullAddress: to)!
+        return AlphaWallet.Address(uncheckedAgainstNullAddress: to)
     }
 
     enum CodingKeys: String, CodingKey {
@@ -94,8 +100,7 @@ extension Transaction {
             let amount = BigInt(amount1, radix: 16)
             //Extract the address and strip the first 12 (x2 = 24) characters of 0s
             let to = "0x\(transaction.input[transaction.input.index(transaction.input.startIndex, offsetBy: 10 + 24)..<transaction.input.index(transaction.input.startIndex, offsetBy: 10 + 64)])"
-            if let amount = amount, let to = AlphaWallet.Address(string: to)?.eip55String {
-                let contract = transaction.toAddress
+            if let amount = amount, let contract = transaction.toAddress, let to = AlphaWallet.Address(string: to)?.eip55String {
                 if let token = tokensStorage.token(forContract: contract) {
                     let operationType = mapTokenTypeToTransferOperationType(token.type)
                     let result = LocalizedOperationObject(from: transaction.from, to: to, contract: contract, type: operationType.rawValue, value: String(amount), symbol: token.symbol, name: token.name, decimals: token.decimals)
