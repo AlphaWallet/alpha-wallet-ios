f01ed1a9e5e426f3d56f07d032ee14825503d99c
James Sangalli <j.l.sangalli@gmail.com>
2019-11-19 22:15:54 +1100

    bundle 721 tickets by one

--- uefa/RPC/Commands/web3swift-pod/GetERC721ForTicketsBalance.swift
+++
@@ -1,8 +0,0 @@
-// Copyright © 2019 Stormbird PTE. LTD.
-
-import Foundation
-
-struct GetERC721ForTicketsBalance {
-    let abi = "[{\"constant\":true,\"inputs\":[{\"name\":\"_owner\",\"type\":\"address\"}],\"name\":\"getBalances\",\"outputs\":[{\"name\":\"\",\"type\":\"uint256[]\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"}]"
-    let name = "getBalances"
-}
