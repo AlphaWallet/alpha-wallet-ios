5697d3ff4f0a181212c424ac0312fe7b03d63e19
Hwee-Boon Yar <hboon@motionobj.com>
2019-11-27 10:39:26 +0800

    Separate out Infura key into a separate file so it is easy to specify a different key (file) locally when building

---
+++ AlphaWallet/Settings/Types/Constants+Credentials.swift
@@ -0,0 +1,9 @@
+// Copyright © 2019 Stormbird PTE. LTD.
+
+import Foundation
+
+extension Constants {
+    enum Credentials {
+        static let infuraKey = "da3717f25f824cc1baa32d812386d93f"
+    }
+}
