ab71fd064e5b3b215e711006d4690c35e00371b3
Hwee-Boon Yar <hboon@motionobj.com>
2020-01-03 12:28:16 +0800

    Add support for ENS reverse lookup and show ENS name beside addresses in wallet list screen

---
+++ AlphaWallet/RPC/Commands/web3swift-pod/ENSReverseLookupEncode.swift
@@ -0,0 +1,8 @@
+// Copyright © 2019 Stormbird PTE. LTD.
+
+import Foundation
+
+struct ENSReverseLookupEncode {
+    let abi = "[ { \"constant\": false, \"inputs\": [ { \"name\": \"node\", \"type\": \"bytes32\" } ], \"name\": \"name\", \"outputs\": [ { \"name\": \"\", \"type\": \"string\" } ], \"payable\": false, \"stateMutability\": \"nonpayable\", \"type\": \"function\" } ]"
+    let name = "name"
+}
