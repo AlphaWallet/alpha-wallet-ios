7d957c25730d91bfdfaa5e1decc52ae469a3cd82
Hwee-Boon Yar <hboon@motionobj.com>
2020-04-13 13:26:29 +0800

    Fix: rate limited code using `RateLimiter` doesn't fire during busy periods

--- AlphaWallet/Core/Types/RateLimiter.swift
+++ AlphaWallet/Core/Types/RateLimiter.swift
@@ -2,23 +2,43 @@
 
 import Foundation
 
-///One limitation of this class due to simplification: if "requests" keep coming in, each with the time limit from the last, the block will not fire until one of the request gets a breather of `limit`
 class RateLimiter {
+    private let name: String?
     private let block: () -> Void
     private let limit: TimeInterval
     private var timer: Timer?
+    private var shouldRunWhenWindowCloses = false
+    private var isWindowActive: Bool {
+        timer?.isValid ?? false
+    }
 
-    init(limit: TimeInterval, block: @escaping () -> Void) {
+    init(name: String? = nil, limit: TimeInterval, autoRun: Bool = false, block: @escaping () -> Void) {
+        self.name = name
         self.limit = limit
         self.block = block
+        if autoRun {
+            run()
+        }
     }
 
     func run() {
-        timer?.invalidate()
-        timer = Timer.scheduledTimer(timeInterval: limit, target: self, selector: #selector(runBlock), userInfo: nil, repeats: false)
+        if isWindowActive {
+            shouldRunWhenWindowCloses = true
+        } else {
+            runWithNewWindow()
+        }
     }
 
-    @objc private func runBlock() {
+    @objc private func windowIsClosed() {
+        if shouldRunWhenWindowCloses {
+            runWithNewWindow()
+        }
+    }
+
+    private func runWithNewWindow() {
+        shouldRunWhenWindowCloses = false
         block()
+        timer?.invalidate()
+        timer = Timer.scheduledTimer(timeInterval: limit, target: self, selector: #selector(windowIsClosed), userInfo: nil, repeats: false)
     }
 }
