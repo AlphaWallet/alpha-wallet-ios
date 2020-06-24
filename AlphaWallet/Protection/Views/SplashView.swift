d64b2e6f49eac0383e876e21da3e3def957a9376
hadwanihardik@gmail.com <hadwanihardik@gmail.com>
2020-06-24 17:24:34 +0530

    Resolved issue : #1897

--- AlphaWallet/Protection/Views/SplashView.swift
+++ AlphaWallet/Protection/Views/SplashView.swift
@@ -3,18 +3,30 @@
 import UIKit
 
 class SplashView: UIView {
+    let logoImageView = UIImageView(image: R.image.launch_icon())
+
     init() {
         super.init(frame: CGRect.zero)
         backgroundColor = .white
-        let logoImageView = UIImageView(image: R.image.launch_icon())
         logoImageView.translatesAutoresizingMaskIntoConstraints = false
         addSubview(logoImageView)
-        NSLayoutConstraint.activate([
-            logoImageView.centerXAnchor.constraint(equalTo: centerXAnchor),
-            logoImageView.centerYAnchor.constraint(equalTo: centerYAnchor),
-        ])
+        self.layoutSubviews()
     }
-
+    override func layoutSubviews() {
+        if let topPadding = UIApplication.shared.keyWindow?.safeAreaInsets.top, topPadding > 24 {
+            let guide = self.safeAreaLayoutGuide
+            NSLayoutConstraint.activate([
+                logoImageView.centerXAnchor.constraint(equalTo: guide.centerXAnchor),
+                logoImageView.centerYAnchor.constraint(equalTo: guide.centerYAnchor),
+            ])
+        } else {
+            let guide = self
+            NSLayoutConstraint.activate([
+                logoImageView.centerXAnchor.constraint(equalTo: guide.centerXAnchor),
+                logoImageView.centerYAnchor.constraint(equalTo: guide.centerYAnchor),
+            ])
+        }
+    }
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
