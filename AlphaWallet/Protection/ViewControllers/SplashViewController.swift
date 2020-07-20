42bfa394d3e715586ce6506cf19bc45e79d6e2c5
Vladyslav shepitko <vladyslav.shepitko@gmail.com>
2020-07-20 12:11:27 +0300

    App icon in launch (aka splash) screen should not move (up) abruptly #1897

--- AlphaWallet/Protection/ViewControllers/SplashViewController.swift
+++ AlphaWallet/Protection/ViewControllers/SplashViewController.swift
@@ -4,21 +4,32 @@ import Foundation
 import UIKit
 
 class SplashViewController: UIViewController {
-    private var splashView = SplashView()
 
     init() {
         super.init(nibName: nil, bundle: nil)
-        splashView.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(splashView)
-        NSLayoutConstraint.activate([
-            splashView.topAnchor.constraint(equalTo: view.topAnchor),
-            splashView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            splashView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-            splashView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-        ])
     }
 
     required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        return nil
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let controller = UIStoryboard(name: "LaunchScreen", bundle: nil).instantiateInitialViewController() {
+            addChild(controller)
+
+            view.addSubview(controller.view)
+            view.translatesAutoresizingMaskIntoConstraints = false
+
+            NSLayoutConstraint.activate([
+                controller.view.topAnchor.constraint(equalTo: view.topAnchor),
+                controller.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+                controller.view.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+                controller.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            ])
+            
+            controller.didMove(toParent: self)
+        }
     }
 }
