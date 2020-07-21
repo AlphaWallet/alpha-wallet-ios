7de49e836d2921e94ecaad572573fd17edb30bfb
Vladyslav shepitko <vladyslav.shepitko@gmail.com>
2020-07-21 08:19:09 +0300

    Add Customer Support channel to Settings #2051

--- AlphaWallet/Settings/Types/ServiceProvider.swift
+++ AlphaWallet/Settings/Types/ServiceProvider.swift
@@ -4,25 +4,34 @@ import Foundation
 import UIKit
 
 enum URLServiceProvider {
-    case telegram
+    case telegramPublic
+    case telegramCustomer
     case twitter
     case reddit
     case facebook
 
     var title: String {
         switch self {
-        case .telegram: return "Telegram"
-        case .twitter: return "Twitter"
-        case .reddit: return "Reddit"
-        case .facebook: return "Facebook"
+        case .telegramPublic:
+            return "Telegram (Public Channel)"
+        case .telegramCustomer:
+            return "Telegram (Customer Support)"
+        case .twitter:
+            return "Twitter"
+        case .reddit:
+            return "Reddit"
+        case .facebook:
+            return "Facebook"
         }
     }
 
     //TODO should probably change or remove `localURL` since iOS supports deep links now
     var localURL: URL? {
         switch self {
-        case .telegram:
+        case .telegramPublic:
             return URL(string: "https://t.me/AlphaWalletGroup")!
+        case .telegramCustomer:
+            return URL(string: "https://t.me/AlphaWalletSupport")!
         case .twitter:
             return URL(string: "twitter://user?screen_name=\(Constants.twitterUsername)")!
         case .reddit:
@@ -33,28 +42,30 @@ enum URLServiceProvider {
     }
 
     var remoteURL: URL {
-        return URL(string: remoteURLString)!
-    }
-
-    private var remoteURLString: String {
         switch self {
-        case .telegram:
-            return "https://t.me/AlphaWalletGroup"
+        case .telegramPublic:
+            return URL(string: "https://t.me/AlphaWalletGroup")!
+        case .telegramCustomer:
+            return URL(string: "https://t.me/AlphaWalletSupport")!
         case .twitter:
-            return "https://twitter.com/\(Constants.twitterUsername)"
+            return URL(string: "https://twitter.com/\(Constants.twitterUsername)")!
         case .reddit:
-            return "https://reddit.com/\(Constants.redditGroupName)"
+            return URL(string: "https://reddit.com/\(Constants.redditGroupName)")!
         case .facebook:
-            return "https://www.facebook.com/\(Constants.facebookUsername)"
+            return URL(string: "https://www.facebook.com/\(Constants.facebookUsername)")!
         }
     }
 
     var image: UIImage? {
         switch self {
-        case .telegram: return R.image.settings_telegram()
-        case .twitter: return R.image.settings_twitter()
-        case .reddit: return R.image.settings_reddit()
-        case .facebook: return R.image.settings_facebook()
+        case .telegramPublic, .telegramCustomer:
+            return R.image.settings_telegram()
+        case .twitter:
+            return R.image.settings_twitter()
+        case .reddit:
+            return R.image.settings_reddit()
+        case .facebook:
+            return R.image.settings_facebook()
         }
     }
 }
