fcfbb6427cd40d53b5786a00cd1c0a79208ba309
James Sangalli <j.l.sangalli@gmail.com>
2020-09-07 10:18:28 +1000

    increase gas price to 500 gwei which is the limit set by GETH

--- AlphaWallet/Transfer/Types/GasPriceConfiguration.swift
+++ AlphaWallet/Transfer/Types/GasPriceConfiguration.swift
@@ -8,6 +8,6 @@ public struct GasPriceConfiguration {
     static let defaultPrice: BigInt = EtherNumberFormatter.full.number(from: "9", units: UnitConfiguration.gasPriceUnit)!
     static let minPrice: BigInt = EtherNumberFormatter.full.number(from: "1", units: UnitConfiguration.gasPriceUnit)!
     static let oneGwei: BigInt = EtherNumberFormatter.full.number(from: "1", units: UnitConfiguration.gasPriceUnit)!
-    static let maxPrice: BigInt = EtherNumberFormatter.full.number(from: "300", units: UnitConfiguration.gasPriceUnit)!
+    static let maxPrice: BigInt = EtherNumberFormatter.full.number(from: "500", units: UnitConfiguration.gasPriceUnit)!
     static let xDaiGasPrice: BigInt = EtherNumberFormatter.full.number(from: "1", units: UnitConfiguration.gasPriceUnit)!
 }
