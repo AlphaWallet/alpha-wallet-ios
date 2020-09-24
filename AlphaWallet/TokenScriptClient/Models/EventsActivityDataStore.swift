ae6d9fb3b4be65a3151a44422ff0ef93e4a17311
Hwee-Boon Yar <hboon@motionobj.com>
2020-09-24 12:06:19 +0800

    Speed up Activities by only ever loading a subset of activities. Pagination is not supported.
    Supporting pagination is a lot more work. It will have to handle:
    
    * Fetching of new activities
    * Refresh of activities
    * Merging with paginated transactions

--- AlphaWallet/TokenScriptClient/Models/EventsActivityDataStore.swift
+++ AlphaWallet/TokenScriptClient/Models/EventsActivityDataStore.swift
@@ -7,9 +7,13 @@ protocol EventsActivityDataStoreProtocol {
     func add(events: [EventActivity], forTokenContract contract: AlphaWallet.Address)
     func getMatchingEventsSortedByBlockNumber(forContract contract: AlphaWallet.Address, tokenContract: AlphaWallet.Address, server: RPCServer, eventName: String) -> [EventActivity]
     func getEvents(forContract contract: AlphaWallet.Address, forEventName eventName: String, filter: String, server: RPCServer) -> [EventActivity]
+    func getRecentEvents() -> [EventActivity]
 }
 
 class EventsActivityDataStore: EventsActivityDataStoreProtocol {
+    //For performance. Fetching and displaying 10k activities stalls the app for a few seconds. We just keep it simple, no pagination. Pagination is complicated because we have to handle re-fetching of activities, updates as well as blending with transactions
+    static let numberOfActivitiesToUse = 1000
+
     private let realm: Realm
 
     init(realm: Realm) {
@@ -31,6 +35,13 @@ class EventsActivityDataStore: EventsActivityDataStoreProtocol {
                 .filter("chainId = \(server.chainID)")
                 .filter("eventName = '\(eventName)'")
                 .filter("filter = '\(filter)'"))
+    }
+
+    func getRecentEvents() -> [EventActivity] {
+        return Array(realm.objects(EventActivity.self)
+                .sorted(byKeyPath: "date", ascending: false)
+                .prefix(Self.numberOfActivitiesToUse)
+        )
     }
 
     private func delete<S: Sequence>(events: S) where S.Element: EventActivity {
