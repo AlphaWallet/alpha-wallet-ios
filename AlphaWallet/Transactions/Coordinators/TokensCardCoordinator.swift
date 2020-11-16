079a415932b22b0e3020e75e4811a7dfb97c1719
Vladyslav shepitko <vladyslav.shepitko@gmail.com>
2020-11-16 09:57:39 +0200

    Track events for actionsheet-style confirmation for "normal" transactions #2199

--- AlphaWallet/Transactions/Coordinators/TokensCardCoordinator.swift
+++ AlphaWallet/Transactions/Coordinators/TokensCardCoordinator.swift
@@ -656,7 +656,7 @@ extension TokensCardCoordinator: TransferTokensCardViaWalletAddressViewControlle
             switch paymentFlow {
             case .send:
                 if case .send(let transferType) = paymentFlow {
-                    let coordinator = TransferNFTCoordinator(navigationController: navigationController, transferType: transferType, tokenHolder: tokenHolder, recipient: recipient, keystore: keystore, session: session)
+                    let coordinator = TransferNFTCoordinator(navigationController: navigationController, transferType: transferType, tokenHolder: tokenHolder, recipient: recipient, keystore: keystore, session: session, analyticsCoordinator: analyticsCoordinator)
                     addCoordinator(coordinator)
                     coordinator.delegate = self
                     coordinator.start()
@@ -698,7 +698,7 @@ extension TokensCardCoordinator: TokenInstanceActionViewControllerDelegate {
     func confirmTransactionSelected(in viewController: TokenInstanceActionViewController, tokenObject: TokenObject, contract: AlphaWallet.Address, tokenId: TokenId, values: [AttributeId: AssetInternalValue], localRefs: [AttributeId: AssetInternalValue], server: RPCServer, session: WalletSession, keystore: Keystore, transactionFunction: FunctionOrigin) {
         switch transactionFunction.makeUnConfirmedTransaction(withTokenObject: tokenObject, tokenId: tokenId, attributeAndValues: values, localRefs: localRefs, server: server, session: session) {
         case .success(let transaction):
-            let coordinator = TransactionConfirmationCoordinator(navigationController: navigationController, session: session, transaction: transaction, configuration: .tokenScriptTransaction(confirmType: .signThenSend, contract: contract, keystore: keystore))
+            let coordinator = TransactionConfirmationCoordinator(navigationController: navigationController, session: session, transaction: transaction, configuration: .tokenScriptTransaction(confirmType: .signThenSend, contract: contract, keystore: keystore), analyticsCoordinator: analyticsCoordinator)
             coordinator.delegate = self
             addCoordinator(coordinator)
             coordinator.start()
