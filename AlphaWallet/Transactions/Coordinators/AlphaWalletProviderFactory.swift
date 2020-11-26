3c14d1ef5f4ee615155f69d48a7e872cd80d5190
Vladyslav shepitko <vladyslav.shepitko@gmail.com>
2020-11-26 16:20:28 +0200

    refactor calling requests using promises instead of completion closure

--- AlphaWallet/Transactions/Coordinators/AlphaWalletProviderFactory.swift
+++ AlphaWallet/Transactions/Coordinators/AlphaWalletProviderFactory.swift
@@ -31,3 +31,24 @@ extension MoyaProvider {
         }
     }
 }
+
+func attempt<T>(maximumRetryCount: Int = 3, delayBeforeRetry: DispatchTimeInterval = .seconds(1), _ body: @escaping () -> Promise<T>) -> Promise<T> {
+    var attempts = 0
+    func attempt() -> Promise<T> {
+        attempts += 1
+
+        return body().recover { error -> Promise<T> in
+            guard attempts < maximumRetryCount else {
+                throw error
+            }
+
+            if case PMKError.cancelled = error {
+                throw error
+            }
+
+            return after(delayBeforeRetry).then(on: nil, attempt)
+        }
+    }
+
+    return attempt()
+}
