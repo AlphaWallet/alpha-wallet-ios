f30f655c035be7a53955da9d05907da5c68144be
Vladyslav shepitko <vladyslav.shepitko@gmail.com>
2020-11-26 14:00:26 +0200

    Add a promise-version of the `request()` function to `MoyaProvider` and use it #2321

--- AlphaWallet/Transactions/Coordinators/AlphaWalletProviderFactory.swift
+++ AlphaWallet/Transactions/Coordinators/AlphaWalletProviderFactory.swift
@@ -3,6 +3,7 @@
 import Alamofire
 import Foundation
 import Moya
+import PromiseKit
 
 struct AlphaWalletProviderFactory {
     static let policies: [String: ServerTrustPolicy] = [:]
@@ -13,5 +14,20 @@ struct AlphaWalletProviderFactory {
             serverTrustPolicyManager: ServerTrustPolicyManager(policies: policies)
         )
         return MoyaProvider<AlphaWalletService>(manager: manager)
+    }
+}
+
+extension MoyaProvider {
+    func request(_ target: Target, callbackQueue: DispatchQueue? = .none, progress: ProgressBlock? = .none) -> Promise<Moya.Response> {
+        Promise { seal in
+            request(target, callbackQueue: callbackQueue, progress: progress) { result in
+                switch result {
+                case .success(let response):
+                    seal.fulfill(response)
+                case .failure(let error):
+                    seal.reject(error)
+                }
+            }
+        }
     }
 }
