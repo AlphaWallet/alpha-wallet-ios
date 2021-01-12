59aaff7ccb62a3acae6a65326ab4046fa13e82c4
Hwee-Boon Yar <hboon@motionobj.com>
2021-01-12 16:09:44 +0800

    Display more user-friendly message when there's not enough funds for sending a transaction instead of: "APIKit.Session-TaskError error 2"

--- AlphaWallet/Extensions/Session+PromiseKit.swift
+++ AlphaWallet/Extensions/Session+PromiseKit.swift
@@ -2,7 +2,14 @@
 
 import Foundation
 import APIKit
+import JSONRPCKit
 import PromiseKit
+
+struct InsufficientFundsError: LocalizedError {
+    var errorDescription: String? {
+        R.string.localizable.configureTransactionNotEnoughFunds()
+    }
+}
 
 extension Session {
     class func send<Request: APIKit.Request>(_ request: Request, callbackQueue: CallbackQueue? = nil) -> Promise<Request.Response> {
@@ -12,6 +19,14 @@ extension Session {
                 case .success(let result):
                     seal.fulfill(result)
                 case .failure(let error):
+                    if case let .responseError(JSONRPCError.responseError(e)) = error {
+                        if e.message.hasPrefix("Insufficient funds") {
+                            seal.reject(InsufficientFundsError())
+                            return
+                        } else {
+                            //no-op
+                        }
+                    }
                     seal.reject(error)
                 }
             }
