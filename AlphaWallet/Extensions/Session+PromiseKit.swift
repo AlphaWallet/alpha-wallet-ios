7e22c71b133ae635fd949934b960ea6c97764500
Hwee-Boon Yar <hboon@motionobj.com>
2021-02-25 16:07:12 +0800

    Log RPC node error messages (to build a list to improve them later)

--- AlphaWallet/Extensions/Session+PromiseKit.swift
+++ AlphaWallet/Extensions/Session+PromiseKit.swift
@@ -14,6 +14,9 @@ struct InsufficientFundsError: LocalizedError {
 struct PossibleBinanceTestnetTimeoutError: LocalizedError {
 }
 
+struct RateLimitError: LocalizedError {
+}
+
 extension Session {
     class func send<Request: APIKit.Request>(_ request: Request, callbackQueue: CallbackQueue? = nil) -> Promise<Request.Response> {
         Promise { seal in
@@ -23,21 +26,41 @@ extension Session {
                     seal.fulfill(result)
                 case .failure(let error):
                     if case let .responseError(JSONRPCError.responseError(_, message: message, _)) = error {
+                        RemoteLogger.instance.logRpcOrOtherWebError(message, url: request.baseURL.absoluteString)
                         if message.lowercased().hasPrefix("insufficient funds") {
                             seal.reject(InsufficientFundsError())
-                            return
                         } else {
-                            //no-op
+                            seal.reject(error)
                         }
+                        return
                     }
+
+                    if case let SessionTaskError.responseError(APIKit.ResponseError.unacceptableStatusCode(statusCode)) = error {
+                        RemoteLogger.instance.logRpcOrOtherWebError("\(error.localizedDescription) | status: \(statusCode)", url: request.baseURL.absoluteString)
+                        if statusCode == 429 {
+                            seal.reject(RateLimitError())
+                        } else {
+                            seal.reject(error)
+                        }
+                        return
+                    }
+
                     if case let SessionTaskError.responseError(e) = error, RPCServer.binance_smart_chain_testnet.rpcURL.absoluteString == request.baseURL.absoluteString, e.localizedDescription == "The data couldn’t be read because it isn’t in the correct format." {
                         //This is potentially Binance testnet timing out
                         seal.reject(PossibleBinanceTestnetTimeoutError())
                         return
                     }
+
+                    RemoteLogger.instance.logRpcOrOtherWebError(error.localizedDescription, url: request.baseURL.absoluteString)
                     seal.reject(error)
                 }
             }
         }
+    }
+}
+
+extension RPCServer {
+    static func serverWithRpcURL(_ string: String) -> RPCServer? {
+        RPCServer.allCases.first { $0.rpcURL.absoluteString == string }
     }
 }
--- AlphaWallet/Settings/Types/Constants+Credentials.swift
+++ AlphaWallet/Settings/Types/Constants+Credentials.swift
@@ -7,5 +7,6 @@ extension Constants {
         static let infuraKey = "da3717f25f824cc1baa32d812386d93f"
         static let etherscanKey = "1PX7RG8H4HTDY8X55YRMCAKPZK476M23ZR"
         static let analyticsKey = ""
+        static let paperTrail = (host: "", port: UInt(0))
     }
 }
