a09300131087d1d2bcfe04ffff31277aebf71cd9
Vladyslav shepitko <vladyslav.shepitko@gmail.com>
2021-03-03 11:43:58 +0200

    Add "Show Seed Phrase" to Settings tab #2582

--- AlphaWallet/Wallet/ViewModels/ShowSeedPhraseIntroductionViewModel.swift
+++ AlphaWallet/Wallet/ViewModels/ShowSeedPhraseIntroductionViewModel.swift
@@ -1,23 +1,28 @@
-// Copyright © 2019 Stormbird PTE. LTD.
+//
+//  ShowSeedPhraseIntroductionViewModel.swift
+//  AlphaWallet
+//
+//  Created by Vladyslav Shepitko on 03.03.2021.
+//
 
-import Foundation
 import UIKit
 
-struct SeedPhraseBackupIntroductionViewModel {
+struct ShowSeedPhraseIntroductionViewModel {
+
     var backgroundColor: UIColor {
         return Colors.appWhite
     }
 
     var title: String {
-        return R.string.localizable.walletsBackupHdWalletIntroductionButtonTitle()
-    } 
+        return R.string.localizable.walletsShowSeedPhraseIntroductionButtonTitle()
+    }
 
     var imageViewImage: UIImage {
-        return R.image.hdIntroduction()!
+        return R.image.showSeedPhraseIntroduction()!
     }
-    
+
     var attributedSubtitle: NSAttributedString {
-        let subtitle = R.string.localizable.walletsBackupHdWalletIntroductionTitle()
+        let subtitle = R.string.localizable.walletsShowSeedPhraseIntroductionTitle()
         let attributeString = NSMutableAttributedString(string: subtitle)
         let style = NSMutableParagraphStyle()
         style.alignment = .center
@@ -26,25 +31,30 @@ struct SeedPhraseBackupIntroductionViewModel {
         attributeString.addAttributes([
             .paragraphStyle: style,
             .font: Screen.Backup.subtitleFont,
-            .foregroundColor: R.color.black()!
+            .foregroundColor: R.color.black()!,
+            .kern: 0.0
         ], range: NSRange(location: 0, length: subtitle.count))
-        
+
         return attributeString
     }
-    
+
     var attributedDescription: NSAttributedString {
-        let description = R.string.localizable.walletsShowSeedPhraseSubtitle()
-        let attributeString = NSMutableAttributedString(string: description)
+        let description = R.string.localizable.walletsShowSeedPhraseIntroductionSubtitle()
+        let attributedString = NSMutableAttributedString(string: description)
         let style = NSMutableParagraphStyle()
         style.alignment = .center
         style.lineSpacing = ScreenChecker.size(big: 18, medium: 14, small: 7)
-        
-        attributeString.addAttributes([
+
+        attributedString.addAttributes([
             .paragraphStyle: style,
             .font: Screen.Backup.descriptionFont,
-            .foregroundColor: Colors.appText
+            .foregroundColor: Colors.appText,
+            .kern: 0.0
         ], range: NSRange(location: 0, length: description.count))
-        
-        return attributeString
-    } 
+
+        attributedString.addAttribute(.font, value: Screen.Backup.descriptionBoldFont, range: NSRange(location: 17, length: 5))
+
+        return attributedString
+    }
+
 }
