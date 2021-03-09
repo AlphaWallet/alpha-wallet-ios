d4ba4c49bc725272b5349466bba08579307794b7
Vladyslav shepitko <vladyslav.shepitko@gmail.com>
2021-03-09 08:54:20 +0200

    Fix: Send "All Funds" adding decimal separator "," for locales (regions) that use it breaks the send function #2590

--- AlphaWallet/Foundation/StringFormatter.swift
+++ AlphaWallet/Foundation/StringFormatter.swift
@@ -32,18 +32,25 @@ final class StringFormatter {
     ///   - double: double to convert.
     ///   - currencyCode: code of the currency.
     /// - Returns: Currency `String` representation.
-    func currency(with value: Double, and currencyCode: String) -> String {
+    func currency(with value: Double, and currencyCode: String = "") -> String {
         let formatter = currencyFormatter
         formatter.currencyCode = currencyCode
         //Trimming is important because the formatter output for `1.2` becomes "1.2 " (with trailing space) when region = Poland
-        return formatter.string(from: NSNumber(value: value))?.trimmed ?? "\(value)"
+        return (formatter.string(from: NSNumber(value: value))?.trimmed ?? "\(value)").droppedTrailingZeros
     }
-
-    func currency(with value: NSDecimalNumber, and currencyCode: String) -> String {
+    /// Converts a NSDecimalNumber to a `currency String`.
+    ///
+    /// - Parameters:
+    ///   - double: double to convert.
+    ///   - currencyCode: code of the currency.
+    /// - Returns: Currency `String` representation.
+    func currency(with value: NSDecimalNumber, and currencyCode: String = "", usesGroupingSeparator: Bool = true) -> String {
         let formatter = currencyFormatter
         formatter.currencyCode = currencyCode
+        formatter.usesGroupingSeparator = usesGroupingSeparator
+
         //Trimming is important because the formatter output for `1.2` becomes "1.2 " (with trailing space) when region = Poland
-        return formatter.string(from: value)?.trimmed ?? "\(value)"
+        return (formatter.string(from: value)?.trimmed ?? "\(value)").droppedTrailingZeros
     }
     /// Converts a Double to a `String`.
     ///
@@ -63,12 +70,14 @@ final class StringFormatter {
         return String(format: "%f", double)
     }
 
-    func alternateAmount(value: NSDecimalNumber) -> String {
+    func alternateAmount(value: NSDecimalNumber, usesGroupingSeparator: Bool = false) -> String {
+        alternateAmountFormatter.usesGroupingSeparator = usesGroupingSeparator
+
         //For some reasons formatter adds trailing whitespace
         if let value = alternateAmountFormatter.string(from: value) {
-            return value.trimmingCharacters(in: .whitespacesAndNewlines)
+            return value.trimmingCharacters(in: .whitespacesAndNewlines).droppedTrailingZeros
         } else {
-            return value.stringValue
+            return value.stringValue.droppedTrailingZeros
         } 
     }
 }
