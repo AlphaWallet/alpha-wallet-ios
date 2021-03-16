57b5b1d14d87129d32f5df3089ec7fab25a34023
Hwee-Boon Yar <hboon@motionobj.com>
2021-03-16 12:41:38 +0800

    Log to analytics WalletConnect

--- AlphaWallet/WalletConnect/Controllers/WalletConnectToSessionCoordinatorBridgeToPromise.swift
+++ AlphaWallet/WalletConnect/Controllers/WalletConnectToSessionCoordinatorBridgeToPromise.swift
@@ -12,10 +12,10 @@ private class WalletConnectToSessionCoordinatorBridgeToPromise {
     private let (promiseToReturn, seal) = Promise<WalletConnectServer.ConnectionChoice>.pending()
     private var retainCycle: WalletConnectToSessionCoordinatorBridgeToPromise?
 
-    init(navigationController: UINavigationController, coordinator: Coordinator, connection: WalletConnectConnection, serverChoices: [RPCServer]) {
+    init(navigationController: UINavigationController, coordinator: Coordinator, connection: WalletConnectConnection, serverChoices: [RPCServer], analyticsCoordinator: AnalyticsCoordinator) {
         retainCycle = self
 
-        let newCoordinator = WalletConnectToSessionCoordinator(connection: connection, navigationController: navigationController, serverChoices: serverChoices)
+        let newCoordinator = WalletConnectToSessionCoordinator(analyticsCoordinator: analyticsCoordinator, connection: connection, navigationController: navigationController, serverChoices: serverChoices)
         newCoordinator.delegate = self
         coordinator.addCoordinator(newCoordinator)
 
@@ -41,12 +41,13 @@ extension WalletConnectToSessionCoordinatorBridgeToPromise: WalletConnectToSessi
 
 extension WalletConnectToSessionCoordinator {
 
-    static func promise(_ navigationController: UINavigationController, coordinator: Coordinator, connection: WalletConnectConnection, serverChoices: [RPCServer]) -> Promise<WalletConnectServer.ConnectionChoice> {
+    static func promise(_ navigationController: UINavigationController, coordinator: Coordinator, connection: WalletConnectConnection, serverChoices: [RPCServer], analyticsCoordinator: AnalyticsCoordinator) -> Promise<WalletConnectServer.ConnectionChoice> {
         return WalletConnectToSessionCoordinatorBridgeToPromise(
             navigationController: navigationController,
             coordinator: coordinator,
             connection: connection,
-            serverChoices: serverChoices
+            serverChoices: serverChoices,
+            analyticsCoordinator: analyticsCoordinator
         ).promise
     }
 }
