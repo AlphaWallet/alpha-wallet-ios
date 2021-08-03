5986517e1e0e57ecf96a6e5d61efbff006816039
Hwee-Boon Yar <hboon@motionobj.com>
2021-08-03 11:39:24 +0800

    Fix: broken test build

---
+++ AlphaWalletTests/Activitities/FakeActivitiesService.swift
@@ -0,0 +1,12 @@
+// Copyright © 2021 Stormbird PTE. LTD.
+
+@testable import AlphaWallet
+
+class FakeActivitiesService: ActivitiesServiceType {
+    var subscribableViewModel: Subscribable<ActivitiesViewModel> { .init(nil) }
+    var subscribableUpdatedActivity: Subscribable<Activity> { .init(nil) }
+
+    func stop() {}
+    func reinject(activity: Activity) {}
+    func copy(activitiesFilterStrategy: ActivitiesFilterStrategy, transactionsFilterStrategy: TransactionsFilterStrategy) -> ActivitiesServiceType { self }
+}
