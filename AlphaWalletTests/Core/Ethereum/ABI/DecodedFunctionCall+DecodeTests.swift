985c1e89789f62c5ac14f37d09b41aefd34a38ad
Hwee-Boon Yar <hboon@motionobj.com>
2021-08-06 12:23:02 +0800

    Fix: crash in transaction data decoding

---
+++ AlphaWalletTests/Core/Ethereum/ABI/DecodedFunctionCall+DecodeTests.swift
@@ -0,0 +1,19 @@
+// Copyright © 2021 Stormbird PTE. LTD.
+
+import XCTest
+@testable import AlphaWallet
+
+class DecodedFunctionCallTest: XCTestCase {
+    func testDecode() {
+        let data = Data(hex: "0xa9059cbb0000000000000000000000003e0763abd685b61e6f001ed33601053401415c520000000000000000000000000000000000000000000000000000000153f4cf00")
+        let decoded = DecodedFunctionCall.decode(data: data, abi: AlphaWallet.Ethereum.ABI.ERC20)
+        XCTAssertEqual(decoded?.name, "transfer")
+    }
+
+    func testDecodeDoesNotCrash() {
+        ////https://ropsten.etherscan.io/tx/0xf406723cc8e0165ded6e8e268e6576ab1a4f12736f6b90eff1adbca87f79a608
+        let data = Data(hex: "000000000000000000000000007bee82bdd9e866b2bd114780a47f2261c684e3000000000000000000000000fe6d4bc2de2d0b0e6fe47f08a28ed52f9d052a020000000000000000000000000000000000000000000000000000000000000001")
+        let decoded = DecodedFunctionCall.decode(data: data, abi: AlphaWallet.Ethereum.ABI.ERC20)
+        XCTAssertNil(decoded)
+    }
+}
