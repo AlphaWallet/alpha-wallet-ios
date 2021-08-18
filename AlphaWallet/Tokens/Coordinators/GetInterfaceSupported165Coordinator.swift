326882a775ef584a20cbeda42c1e6d2dbc72c198
Hwee-Boon Yar <hboon@motionobj.com>
2021-08-18 15:27:33 +0800

    Add promise alternative of function which uses completion block

--- AlphaWallet/Tokens/Coordinators/GetInterfaceSupported165Coordinator.swift
+++ AlphaWallet/Tokens/Coordinators/GetInterfaceSupported165Coordinator.swift
@@ -3,6 +3,7 @@
 //
 
 import Foundation
+import PromiseKit
 import Result
 
 class GetInterfaceSupported165Coordinator {
@@ -15,7 +16,7 @@ class GetInterfaceSupported165Coordinator {
     func getInterfaceSupported165(
             hash: String,
             contract: AlphaWallet.Address,
-            completion: @escaping (Result<Bool, AnyError>) -> Void
+            completion: @escaping (ResultResult<Bool, AnyError>.t) -> Void
     ) {
         let function = GetInterfaceSupported165Encode()
         callSmartContract(withServer: server, contract: contract, functionName: function.name, abiString: function.abi, parameters: [hash] as [AnyObject], timeout: TokensDataStore.fetchContractDataTimeout).done { result in
@@ -26,6 +27,19 @@ class GetInterfaceSupported165Coordinator {
             }
         }.catch {
             completion(.failure(AnyError($0)))
+        }
+    }
+
+    func getInterfaceSupported165(hash: String, contract: AlphaWallet.Address) -> Promise<Bool> {
+        let function = GetInterfaceSupported165Encode()
+        return firstly {
+            callSmartContract(withServer: server, contract: contract, functionName: function.name, abiString: function.abi, parameters: [hash] as [AnyObject], timeout: TokensDataStore.fetchContractDataTimeout)
+        }.map { result in
+            if let supported = result["0"] as? Bool {
+                return supported
+            } else {
+                throw AnyError(ABIError.invalidArgumentType)
+            }
         }
     }
 }
