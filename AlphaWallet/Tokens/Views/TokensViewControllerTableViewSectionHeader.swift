9b82fce8b78fd800cccb3cb4c9f06494ad1ed0ce
Vladyslav shepitko <vladyslav.shepitko@gmail.com>
2021-09-09 15:23:41 +0300

    Make the space between "Wallet Value Number" and the "Portfolio change in last 24h" bigger #3168

--- AlphaWallet/Tokens/Views/TokensViewControllerTableViewSectionHeader.swift
+++ AlphaWallet/Tokens/Views/TokensViewControllerTableViewSectionHeader.swift
@@ -8,33 +8,6 @@ protocol ReusableTableHeaderViewType: UIView, WithReusableIdentifier {
 }
 
 extension TokensViewController {
-    class TableViewSectionHeader: UITableViewHeaderFooterView {
-        var filterView: SegmentedControl? {
-            didSet {
-                guard let filterView = filterView else {
-                    if let oldValue = oldValue {
-                        oldValue.removeFromSuperview()
-                    }
-                    return
-                }
-                filterView.backgroundColor = Colors.appWhite
-                filterView.translatesAutoresizingMaskIntoConstraints = false
-                contentView.addSubview(filterView)
-                NSLayoutConstraint.activate([
-                    filterView.anchorsConstraint(to: contentView),
-                ])
-            }
-        }
-
-        override init(reuseIdentifier: String?) {
-            super.init(reuseIdentifier: reuseIdentifier)
-            contentView.backgroundColor = Colors.appWhite
-        }
-
-        required init?(coder aDecoder: NSCoder) {
-            return nil
-        }
-    }
 
     class GeneralTableViewSectionHeader<T: ReusableTableHeaderViewType>: UITableViewHeaderFooterView {
         var subview: T? {
