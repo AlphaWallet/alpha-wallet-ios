299b3490794387466c5364a35f8d4e76dfbcd616
Hwee-Boon Yar <hboon@motionobj.com>
2021-12-13 13:53:53 +0800

    Do not start What's New flow/UI right after user creates a new wallet, for both new install or existing install, because it crashes with prompt to backup the new wallet

--- AlphaWallet/WhatsNew/Coordinators/WhatsNewExperimentCoordinator.swift
+++ AlphaWallet/WhatsNew/Coordinators/WhatsNewExperimentCoordinator.swift
@@ -8,6 +8,9 @@ protocol WhatsNewExperimentCoordinatorDelegate: AnyObject {
 
 class WhatsNewExperimentCoordinator: Coordinator {
     static private let key = "experiments.whatsnew.1"
+    static private let walletLastCreatedWindowSkipWhatsNew = TimeInterval(3)
+
+    static var lastCreatedWalletTimestamp: Date?
 
     private let navigationController: UINavigationController
     private let viewModel = HelpUsViewModel()
@@ -34,6 +37,10 @@ class WhatsNewExperimentCoordinator: Coordinator {
     }
 
     func start() {
+        //We don't want to show the user What's New right after a wallet is created (not imported) because this will crash with the UI which we used to prompt users to back up the new wallet
+        if let lastCreatedWalletTimestamp = Self.lastCreatedWalletTimestamp, Date().timeIntervalSince(lastCreatedWalletTimestamp) < Self.walletLastCreatedWindowSkipWhatsNew {
+            return
+        }
         guard !hasRan else {
             delegate?.didEnd(in: self)
             return
