a4e471615d2817249b79e13689d042a9cfabd2c5
Krypto Pank <krypto.pank@gmail.com>
2022-01-11 12:57:17 +0200

    Balance incorrect on Add/Hide token page #3645

--- AlphaWallet/Tokens/ViewModels/WalletTokenViewCellViewModel.swift
+++ AlphaWallet/Tokens/ViewModels/WalletTokenViewCellViewModel.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import BigInt
+
 struct WalletTokenViewCellViewModel {
     private let shortFormatter = EtherNumberFormatter.short
     private let token: TokenObject
@@ -28,7 +29,7 @@ struct WalletTokenViewCellViewModel {
     }
 
     var cryptoValueAttributedString: NSAttributedString {
-        return NSAttributedString(string: isVisible ? amount : String(), attributes: [
+        return NSAttributedString(string: String(), attributes: [
             .foregroundColor: Screen.TokenCard.Color.title,
             .font: Screen.TokenCard.Font.title
         ])
