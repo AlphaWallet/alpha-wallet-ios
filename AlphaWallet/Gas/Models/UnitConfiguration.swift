fc6ac6ec4acaeb96cc873f8645bf5dabb49828c6
Hwee-Boon Yar <hboon@motionobj.com>
2022-02-17 12:12:33 +0800

    Refactor. Move type out into its own file

---
+++ AlphaWallet/Gas/Models/UnitConfiguration.swift
@@ -0,0 +1,9 @@
+// Copyright © 2022 Stormbird PTE. LTD.
+
+import Foundation
+
+struct UnitConfiguration {
+    static let gasPriceUnit: EthereumUnit = .gwei
+    static let gasFeeUnit: EthereumUnit = .ether
+    static let finneyUnit: EthereumUnit = .finney
+}
