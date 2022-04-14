411021a18441b5c1370d61a7c3b45cd2e317ccbd
Jerome Chan <eviltofu@gmail.com>
2022-04-14 13:23:54 +0800

    Added code to persist cache for GetIsERC1155ContractCoordinator

---
+++ AlphaWalletTests/Tokens/Coordinators/GetIsERC1155ContractCoordinatorTestCase.swift
@@ -0,0 +1,49 @@
+//
+//  GetIsERC1155ContractCoordinatorTestCase.swift
+//  AlphaWalletTests
+//
+//  Created by Jerome Chan on 14/4/22.
+//
+
+@testable import AlphaWallet
+import AlphaWalletAddress
+import PromiseKit
+import XCTest
+
+class GetIsERC1155ContractCoordinatorTestCase: XCTestCase {
+
+    private let fileName = "test-cache.json"
+    private let address1: AlphaWallet.Address = AlphaWallet.Address(string: "0xbbce83173d5c1d122ae64856b4af0d5ae07fa362")!
+    private var result: Bool?
+
+    override func setUpWithError() throws {
+        let url = try cacheUrlFor(fileName: fileName)
+        if FileManager.default.fileExists(atPath: url.path) && FileManager.default.isDeletableFile(atPath: url.path) {
+            try FileManager.default.removeItem(at: url)
+        }
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testgetIsERC1155Contract() throws {
+        let coordinator = GetIsERC1155ContractCoordinator(forServer: .main, cacheName: fileName)
+        let expectation = expectation(description: "Waiting for server response")
+        coordinator.getIsERC1155Contract(for: address1)
+            .done { returnValue in
+                self.result = returnValue
+                expectation.fulfill()
+            }
+            .cauterize()
+        wait(for: [expectation], timeout: 10 * 60)
+        let cache = CachedERC1155ContractDictionary(fileName: fileName)
+        XCTAssertNotNil(cache)
+        XCTAssertNotNil(result)
+        let cachedResult = cache!.isERC1155Contract(for: address1)
+        XCTAssertNotNil(cachedResult)
+        XCTAssertEqual(cachedResult!, result!)
+        cache!.remove()
+    }
+
+}
---
+++ AlphaWalletTests/Utilities/urlUtilities.swift
@@ -0,0 +1,14 @@
+//
+//  urlUtilities.swift
+//  AlphaWalletTests
+//
+//  Created by Jerome Chan on 14/4/22.
+//
+
+import Foundation
+
+func cacheUrlFor(fileName: String) throws -> URL {
+    var url: URL = try FileManager.default.url(for: .cachesDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+    url.appendPathComponent(fileName)
+    return url
+}
