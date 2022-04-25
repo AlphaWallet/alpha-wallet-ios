6b14ce1990de1ae5ef278cc3953cbad15cb1be5e
Hwee-Boon Yar <hboon@motionobj.com>
2022-04-25 11:30:58 +0800

    Remove spurious question mark in What's New

--- AlphaWallet/WhatsNew/Coordinators/WhatsNewExperimentCoordinator.swift
+++ AlphaWallet/WhatsNew/Coordinators/WhatsNewExperimentCoordinator.swift
@@ -53,7 +53,7 @@ class WhatsNewExperimentCoordinator: Coordinator {
         let listings = WhatsNewListing(listing: [
             WhatsNew(
                     //TODO localize later. Might not keep this longer term
-                    title: "You can get to your wallet address QR code from Siri/Spotlight?",
+                    title: "You can get to your wallet address QR code from Siri/Spotlight",
                     changes: [
                         "1. Go to homescreen",
                         "2. Pull down to reveal search bar",
