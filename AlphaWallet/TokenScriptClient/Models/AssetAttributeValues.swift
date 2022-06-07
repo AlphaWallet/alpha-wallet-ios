a3b43da4ceb14dc78d7ceb3050dfd9c4f7fc2f20
Krypto Pank <krypto.pank@gmail.com>
2022-06-07 18:38:20 +0300

    Make AssetAttributeValues, Subscribable threadsafe #4820

--- AlphaWallet/TokenScriptClient/Models/AssetAttributeValues.swift
+++ AlphaWallet/TokenScriptClient/Models/AssetAttributeValues.swift
@@ -1,18 +1,20 @@
 // Copyright © 2018 Stormbird PTE. LTD.
 
 import Foundation
+import AlphaWalletAddress
 
 //This class makes it easier to listen to when all the attribute values are available (specifically values from smart contract function calls are available asynchronously)
+
 class AssetAttributeValues {
-    private let attributeValues: [AttributeId: AssetInternalValue]
-    private var resolvedAttributeValues = [AttributeId: AssetInternalValue]()
+    private let attributeValues: AtomicDictionary<AttributeId, AssetInternalValue>
+    private let resolvedAttributeValues: AtomicDictionary<AttributeId, AssetInternalValue> = .init()
 
     var isAllResolved: Bool {
         return resolvedAttributeValues.count == attributeValues.count
     }
 
     init(attributeValues: [AttributeId: AssetInternalValue]) {
-        self.attributeValues = attributeValues
+        self.attributeValues = .init(value: attributeValues)
     }
 
     convenience init(attributeValues: [AttributeId: AssetAttributeSyntaxValue]) {
@@ -21,7 +23,7 @@ class AssetAttributeValues {
 
     func resolve(withUpdatedBlock block: @escaping ([AttributeId: AssetInternalValue]) -> Void) -> [AttributeId: AssetInternalValue] {
         var subscribedAttributes = [Subscribable<AssetInternalValue>]()
-        for (name, value) in attributeValues {
+        for (name, value) in attributeValues.values {
             if case .subscribable(let subscribable) = value {
                 if let subscribedValue = subscribable.value {
                     resolvedAttributeValues[name] = subscribedValue
@@ -32,7 +34,7 @@ class AssetAttributeValues {
                         subscribable.subscribe { value in
                             guard let value = value else { return }
                             self.resolvedAttributeValues[name] = value
-                            block(self.resolvedAttributeValues)
+                            block(self.resolvedAttributeValues.values)
                         }
                     }
                 }
@@ -40,6 +42,6 @@ class AssetAttributeValues {
                 resolvedAttributeValues[name] = value
             }
         }
-        return resolvedAttributeValues
+        return resolvedAttributeValues.values
     }
 }
