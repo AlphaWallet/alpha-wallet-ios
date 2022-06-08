eb8443a8be68778483102c805b1d89801e71d449
Krypto Pank <krypto.pank@gmail.com>
2022-06-08 17:33:37 +0300

    [Refactor] Replace passing activities view model with array of activities. #4834

--- AlphaWallet/Tokens/ViewControllers/FungibleTokenViewController.swift
+++ AlphaWallet/Tokens/ViewControllers/FungibleTokenViewController.swift
@@ -126,10 +126,10 @@ class FungibleTokenViewController: UIViewController {
     }
 
     private func subscribeForActivities() {
-        activitiesService.viewModelPublisher
+        activitiesService.activitiesPublisher
             .receive(on: RunLoop.main)
-            .sink { [weak activitiesPageView] viewModel in
-                activitiesPageView?.configure(viewModel: .init(activitiesViewModel: viewModel))
+            .sink { [weak activitiesPageView] activities in
+                activitiesPageView?.configure(viewModel: .init(activitiesViewModel: .init(activities: activities)))
             }.store(in: &cancelable)
     }
 
