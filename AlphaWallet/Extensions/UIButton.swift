683e822aedc0d2929bdb700308a9f559aa988411
Jerome Chan <eviltofu@gmail.com>
2022-07-21 08:53:00 +0800

    Fixed UIButton setBackgroundColor darkMode bug

--- AlphaWallet/Extensions/UIButton.swift
+++ AlphaWallet/Extensions/UIButton.swift
@@ -5,6 +5,10 @@ import UIKit
 
 extension UIButton {
     func setBackgroundColor(_ color: UIColor, forState: UIControl.State) {
+        color.isDynamic ? setDynamicColor(color, forState: forState) : setNonDynamicColor(color, forState: forState)
+    }
+
+    private func setDynamicColor(_ color: UIColor, forState: UIControl.State) {
         let imageAsset = UIImageAsset()
 
         let lightImage = UIImage(color: color.lightMode)!
@@ -16,5 +20,15 @@ extension UIButton {
         imageAsset.register(darkImage, with: darkMode)
 
         setBackgroundImage(imageAsset.image(with: .current), for: state)
+    }
+
+    private func setNonDynamicColor(_ color: UIColor, forState: UIControl.State) {
+        UIGraphicsBeginImageContext(CGSize(width: 1, height: 1))
+        UIGraphicsGetCurrentContext()!.setFillColor(color.cgColor)
+        UIGraphicsGetCurrentContext()!.fill(CGRect(x: 0, y: 0, width: 1, height: 1))
+        let colorImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+
+        setBackgroundImage(colorImage, for: forState)
     }
 }
