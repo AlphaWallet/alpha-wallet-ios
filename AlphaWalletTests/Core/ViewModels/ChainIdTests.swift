13846c28710f7d1ff06c4db8b6d9148a6ecc9c9c
Hwee-Boon Yar <hboon@motionobj.com>
2022-09-01 12:15:03 +0800

    Fix: chain ID for Palm both mainnet and testnet displayed as negative number

---
+++ AlphaWalletTests/Core/ViewModels/ChainIdTests.swift
@@ -0,0 +1,10 @@
+// Copyright © 2022 Stormbird PTE. LTD.
+
+import XCTest
+@testable import AlphaWallet
+
+class ChainIdTests: XCTestCase {
+    func testLargeChainIdDisplay() {
+        XCTAssertEqual(R.string.localizable.chainIDWithPrefix(RPCServer.palm.chainID), "Chain ID: 11,297,108,109")
+    }
+}
