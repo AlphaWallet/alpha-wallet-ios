ffeed86ce0ee06c5320f34e93af2efd649001daa
Hwee-Boon Yar <hboon@motionobj.com>
2022-09-09 16:25:03 +0800

    [Chore] Code formatting

--- AlphaWallet/WalletConnect/Types/WalletConnect.swift
+++ AlphaWallet/WalletConnect/Types/WalletConnect.swift
@@ -6,7 +6,7 @@
 //
 
 import Foundation
-import WalletConnectSwift  
+import WalletConnectSwift
 import AlphaWalletFoundation
 
 typealias WalletConnectV1Request = WalletConnectSwift.Request
@@ -50,7 +50,7 @@ extension AlphaWallet.WalletConnect.Session {
 }
 
 extension AlphaWallet.WalletConnect {
-    
+
     enum ServerEditingAvailability {
         case disabled
         case enabled
