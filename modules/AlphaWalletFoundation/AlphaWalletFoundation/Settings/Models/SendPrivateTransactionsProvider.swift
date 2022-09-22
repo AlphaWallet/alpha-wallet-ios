fdb178956a9e30a89e81c55f8ca2425ecad967dc
Hwee-Boon Yar <hboon@motionobj.com>
2022-09-22 14:21:47 +0800

    Remove support of sending private transactions for Eden on Ropsten to simplify a future change to code (so we don't have to special case for `.ropsten`). Ropsten is deprecated and going away every soon any way

--- modules/AlphaWalletFoundation/AlphaWalletFoundation/Settings/Models/SendPrivateTransactionsProvider.swift
+++ modules/AlphaWalletFoundation/AlphaWalletFoundation/Settings/Models/SendPrivateTransactionsProvider.swift
@@ -18,8 +18,7 @@ public enum SendPrivateTransactionsProvider: String {
         case .eden:
             switch server {
             case .main: return URL(string: "https://api.edennetwork.io/v1/rpc")!
-            case .ropsten: return URL(string: "https://dev-api.edennetwork.io/v1/rpc")!
-            case .xDai, .kovan, .rinkeby, .poa, .sokol, .classic, .callisto, .goerli, .artis_sigma1, .artis_tau1, .binance_smart_chain, .binance_smart_chain_testnet, .custom, .heco, .heco_testnet, .fantom, .fantom_testnet, .avalanche, .avalanche_testnet, .candle, .polygon, .mumbai_testnet, .optimistic, .optimisticKovan, .cronosTestnet, .arbitrum, .arbitrumRinkeby, .palm, .palmTestnet, .phi: return nil
+            case .ropsten, .xDai, .kovan, .rinkeby, .poa, .sokol, .classic, .callisto, .goerli, .artis_sigma1, .artis_tau1, .binance_smart_chain, .binance_smart_chain_testnet, .custom, .heco, .heco_testnet, .fantom, .fantom_testnet, .avalanche, .avalanche_testnet, .candle, .polygon, .mumbai_testnet, .optimistic, .optimisticKovan, .cronosTestnet, .arbitrum, .arbitrumRinkeby, .palm, .palmTestnet, .phi: return nil
             case .klaytnCypress, .klaytnBaobabTestnet: return nil
             case .ioTeX, .ioTeXTestnet: return nil
             }
