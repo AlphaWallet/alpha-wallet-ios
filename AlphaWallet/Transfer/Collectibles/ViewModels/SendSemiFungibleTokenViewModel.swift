4cf4f46e58841939f10e7b4a25d3d493af7afffa
Krypto Pank <krypto.pank@gmail.com>
2022-09-28 16:48:43 +0300

    Tweak send nft token screen #5452

--- AlphaWallet/Transfer/Collectibles/ViewModels/SendSemiFungibleTokenViewModel.swift
+++ AlphaWallet/Transfer/Collectibles/ViewModels/SendSemiFungibleTokenViewModel.swift
@@ -1,5 +1,5 @@
 //
-//  TransferTokenBatchCardsViaWalletAddressViewControllerViewModel.swift
+//  SendSemiFungibleTokenViewModel.swift
 //  AlphaWallet
 //
 //  Created by Vladyslav Shepitko on 07.09.2021.
@@ -8,38 +8,58 @@
 import UIKit
 import AlphaWalletFoundation
 
-struct TransferTokenBatchCardsViaWalletAddressViewControllerViewModel {
+final class SendSemiFungibleTokenViewModel {
     let token: Token
     let tokenHolders: [TokenHolder]
-    var availableAmountInt: Int {
-        Int(tokenHolders[0].values.valueIntValue ?? 0)
-    }
-    var selectedAmount: Int {
-        tokenHolders[0].selectedCount(tokenId: tokenHolders[0].tokenId) ?? 0
-    }
-    lazy var selectionViewModel: SelectTokenCardAmountViewModel = .init(availableAmount: availableAmountInt, selectedAmount: selectedAmount)
 
-    var navigationTitle: String {
-        R.string.localizable.send()
+    lazy var selectionViewModel: SelectTokenCardAmountViewModel = {
+        let availableAmountInt = Int(tokenHolders[0].values.valueIntValue ?? 0)
+        let selectedAmount: Int = tokenHolders[0].selectedCount(tokenId: tokenHolders[0].tokenId) ?? 0
+
+        return .init(availableAmount: availableAmountInt, selectedAmount: selectedAmount)
+    }()
+
+    let navigationTitle: String = R.string.localizable.send()
+
+    var assetsHeaderViewModel: SendViewSectionHeaderViewModel {
+        let title: String
+        switch token.type {
+        case .erc1155:
+            title = R.string.localizable.semifungiblesSelectedTokens()
+        case .erc721, .erc721ForTickets, .erc875:
+            title = R.string.localizable.semifungiblesAssetsTitle()
+        case .erc20, .nativeCryptocurrency:
+            title = ""
+        }
+
+        return .init(text: title.uppercased())
     }
 
-    var backgroundColor: UIColor {
-        return Colors.appBackground
+    var amountHeaderViewModel: SendViewSectionHeaderViewModel {
+        return .init(text: R.string.localizable.sendAmount().uppercased())
     }
 
-    var targetAddressAttributedString: NSAttributedString {
-        return .init(string: R.string.localizable.aSendRecipientAddressTitle(), attributes: [
-            .font: Fonts.regular(size: 13),
-            .foregroundColor: R.color.dove()!
-        ])
+    var recipientHeaderViewModel: SendViewSectionHeaderViewModel {
+        return .init(text: R.string.localizable.sendRecipient().uppercased(), showTopSeparatorLine: false)
     }
+
+    let backgroundColor: UIColor = Colors.appBackground
 
     var isAmountSelectionHidden: Bool {
-        tokenHolders.count > 1
+        switch token.type {
+        case .erc1155:
+            return tokenHolders.count > 1
+        case .nativeCryptocurrency, .erc20, .erc721, .erc721ForTickets, .erc875:
+            return true
+        }
+    }
+
+    init(token: Token, tokenHolders: [TokenHolder]) {
+        self.token = token
+        self.tokenHolders = tokenHolders
     }
 
     func updateSelectedAmount(_ value: Int) {
-        //NOTE: safety check
         guard tokenHolders.count == 1 else { return }
         tokenHolders[0].select(with: .token(tokenId: tokenHolders[0].tokenId, amount: value))
     }
