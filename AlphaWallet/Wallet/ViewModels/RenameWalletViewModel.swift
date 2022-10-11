3325729cb449662962356ff8eaff0a1515f835a7
Krypto Pank <krypto.pank@gmail.com>
2022-10-11 13:46:43 +0300

    [Refactor] update wallet screen view model

--- AlphaWallet/Wallet/ViewModels/RenameWalletViewModel.swift
+++ AlphaWallet/Wallet/ViewModels/RenameWalletViewModel.swift
@@ -9,23 +9,24 @@ import Foundation
 import Combine
 import AlphaWalletFoundation
 
-class RenameWalletViewModel {
-    let account: AlphaWallet.Address
+struct RenameWalletViewModelInput {
+    let appear: AnyPublisher<Void, Never>
+    let name: AnyPublisher<String, Never>
+}
 
-    var title: String {
-        return R.string.localizable.settingsWalletRename()
-    }
+struct RenameWalletViewModelOutput {
+    let viewState: AnyPublisher<RenameWalletViewModel.ViewState, Never>
+}
 
-    var saveWalletNameTitle: String {
-        return R.string.localizable.walletRenameSave()
-    }
-
-    var walletNameTitle: String {
-        return R.string.localizable.walletRenameEnterNameTitle()
-    }
-
+final class RenameWalletViewModel {
+    private let account: AlphaWallet.Address
     private let analytics: AnalyticsLogger
     private let domainResolutionService: DomainResolutionServiceType
+    private var cancelable = Set<AnyCancellable>()
+
+    let title: String = R.string.localizable.settingsWalletRename()
+    let saveWalletNameTitle: String = R.string.localizable.walletRenameSave()
+    let walletNameTitle: String = R.string.localizable.walletRenameEnterNameTitle()
 
     init(account: AlphaWallet.Address, analytics: AnalyticsLogger, domainResolutionService: DomainResolutionServiceType) {
         self.account = account
@@ -33,20 +34,34 @@ class RenameWalletViewModel {
         self.domainResolutionService = domainResolutionService
     }
 
-    func set(walletName: String) {
+    func transform(input: RenameWalletViewModelInput) -> RenameWalletViewModelOutput {
+        input.name
+            .sink { self.set(walletName: $0) }
+            .store(in: &cancelable)
+
+        let assignedName = input.appear.map { _ in FileWalletStorage().name(for: self.account) }
+
+        let resolvedEns = domainResolutionService.resolveEns(address: account)
+            .map { ens -> EnsName? in return ens }
+            .replaceError(with: nil)
+
+        let viewState = Publishers.CombineLatest(assignedName, resolvedEns)
+            .map { RenameWalletViewModel.ViewState(text: $0.0, placeholder: $0.1, title: self.title) }
+            .eraseToAnyPublisher()
+
+        return .init(viewState: viewState)
+    }
+
+    private func set(walletName: String) {
         FileWalletStorage().addOrUpdate(name: walletName, for: account)
         analytics.log(action: Analytics.Action.nameWallet)
     }
+}
 
-    var resolvedEns: AnyPublisher<String?, Never> {
-        return domainResolutionService.resolveEns(address: account)
-            .map { ens -> EnsName? in return ens }
-            .replaceError(with: nil)
-            .eraseToAnyPublisher()
-    }
-
-    var assignedName: AnyPublisher<String?, Never> {
-        let name = FileWalletStorage().name(for: account)
-        return .just(name)
+extension RenameWalletViewModel {
+    struct ViewState {
+        let text: String?
+        let placeholder: String?
+        let title: String
     }
 }
