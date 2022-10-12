c74b1a817c940499404c95aac8396355cc9f75a9
Krypto Pank <krypto.pank@gmail.com>
2022-10-12 13:22:53 +0300

    add analytics param for publisher version of APIKitsession.request

--- modules/AlphaWalletFoundation/AlphaWalletFoundation/Extensions/Publishers/Session+Publishers.swift
+++ modules/AlphaWalletFoundation/AlphaWalletFoundation/Extensions/Publishers/Session+Publishers.swift
@@ -12,13 +12,26 @@ import Combine
 
 extension APIKitSession {
 
-    class func sendPublisher<Request: APIKit.Request>(_ request: Request, server: RPCServer, callbackQueue: CallbackQueue? = nil) -> AnyPublisher<Request.Response, SessionTaskError> {
+    class func sendPublisher<Request: APIKit.Request>(_ request: Request, server: RPCServer, analytics: AnalyticsLogger, callbackQueue: CallbackQueue? = nil) -> AnyPublisher<Request.Response, SessionTaskError> {
         sendImplPublisher(request, server: server, callbackQueue: callbackQueue)
             .retry(times: 2, when: {
                 guard case SessionTaskError.requestError(let e) = $0 else { return false }
+                if let e = e as? RpcNodeRetryableRequestError {
+                    logRpcNodeError(e, analytics: analytics)
+                }
                 return e is RpcNodeRetryableRequestError
-            })
-            .eraseToAnyPublisher()
+            }).eraseToAnyPublisher()
+    }
+
+    private static func logRpcNodeError(_ rpcNodeError: RpcNodeRetryableRequestError, analytics: AnalyticsLogger) {
+        switch rpcNodeError {
+        case .rateLimited(let server, let domainName):
+            analytics.log(error: Analytics.WebApiErrors.rpcNodeRateLimited, properties: [Analytics.Properties.chain.rawValue: server.chainID, Analytics.Properties.domainName.rawValue: domainName])
+        case .invalidApiKey(let server, let domainName):
+            analytics.log(error: Analytics.WebApiErrors.rpcNodeInvalidApiKey, properties: [Analytics.Properties.chain.rawValue: server.chainID, Analytics.Properties.domainName.rawValue: domainName])
+        case .possibleBinanceTestnetTimeout, .networkConnectionWasLost, .invalidCertificate, .requestTimedOut:
+            return
+        }
     }
 
     private class func sendImplPublisher<Request: APIKit.Request>(_ request: Request, server: RPCServer, callbackQueue: CallbackQueue? = nil) -> AnyPublisher<Request.Response, SessionTaskError> {
