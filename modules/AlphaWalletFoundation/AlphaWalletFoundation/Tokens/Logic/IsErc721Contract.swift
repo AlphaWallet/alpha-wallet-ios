8708f4df7cc0fd3421093cd79cfe108bb459937c
Krypto Pank <krypto.pank@gmail.com>
2022-10-24 13:54:56 +0300

    hardcode 0x7Db4de78E6b9A98752B56959611e4cfdA52269D2 as supports `0x80ac58cd` interface #5667

--- modules/AlphaWalletFoundation/AlphaWalletFoundation/Tokens/Logic/IsErc721Contract.swift
+++ modules/AlphaWalletFoundation/AlphaWalletFoundation/Tokens/Logic/IsErc721Contract.swift
@@ -10,10 +10,17 @@ public class IsErc721Contract {
     private let server: RPCServer
 
     private struct DoesNotSupportERC165Querying {
-        static let bitizen = "0xb891c4d89c1bf012f0014f56ce523f248a07f714"
-        static let cryptoSaga = "0xabc7e6c01237e8eef355bba2bf925a730b714d5f"
+        static let bitizen = AlphaWallet.Address(string: "0xb891c4d89c1bf012f0014f56ce523f248a07f714")!
+        static let cryptoSaga = AlphaWallet.Address(string: "0xabc7e6c01237e8eef355bba2bf925a730b714d5f")!
     }
 
+    private struct DevconVISouvenir {
+        static let polygon = AlphaWallet.Address(string: "0x7Db4de78E6b9A98752B56959611e4cfdA52269D2")!
+        static let arbitrum = AlphaWallet.Address(string: "0x7Db4de78E6b9A98752B56959611e4cfdA52269D2")!
+        static let optimism = AlphaWallet.Address(string: "0x7Db4de78E6b9A98752B56959611e4cfdA52269D2")!
+        static let mainnet = AlphaWallet.Address(string: "0x7522dC5A357891B4dAEC194E285551EA5ea66d09")!
+    }
+    
     private struct ERC165Hash {
         static let official = "0x80ac58cd"
         //https://github.com/ethereum/EIPs/commit/d164cb2031503665c7dfbb759272f63c29b2b848
@@ -34,14 +41,14 @@ public class IsErc721Contract {
         firstly {
             .value(server)
         }.then(on: queue, { [weak self, queue] server -> Promise<Bool> in
+            if let value = IsErc721Contract.sureItsErc721(contract: contract) {
+                return .value(value)
+            }
+
             let key = "\(contract.eip55String)-\(server.chainID)"
             if let promise = self?.inFlightPromises[key] {
                 return promise
             } else {
-                if contract.sameContract(as: DoesNotSupportERC165Querying.bitizen) || contract.sameContract(as: DoesNotSupportERC165Querying.cryptoSaga) {
-                    return .value(true)
-                }
-
                 let function = GetInterfaceSupported165Encode()
 
                 let cryptoKittyPromise = callSmartContract(withServer: server, contract: contract, functionName: function.name, abiString: function.abi, parameters: [ERC165Hash.onlyKat] as [AnyObject])
@@ -77,6 +84,22 @@ public class IsErc721Contract {
                 return promise
             }
         })
+    }
 
+    private static func sureItsErc721(contract: AlphaWallet.Address) -> Bool? {
+        let contracts: [AlphaWallet.Address] = [
+            DoesNotSupportERC165Querying.bitizen,
+            DoesNotSupportERC165Querying.cryptoSaga,
+            DevconVISouvenir.mainnet,
+            DevconVISouvenir.arbitrum,
+            DevconVISouvenir.optimism,
+            DevconVISouvenir.polygon
+        ]
+
+        if contracts.contains(contract) {
+            return true
+        }
+
+        return nil
     }
 }
