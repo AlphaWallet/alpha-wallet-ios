49177d40f26bb4c952053ee5b1a2202260d8ed2f
Krypto Pank <krypto.pank@gmail.com>
2022-11-01 10:24:37 +0200

    Fix test suite #5716

--- AlphaWalletTests/Transfer/ViewModels/RequestViewModelTests.swift
+++ AlphaWalletTests/Transfer/ViewModels/RequestViewModelTests.swift
@@ -3,13 +3,26 @@
 import XCTest
 @testable import AlphaWallet
 import AlphaWalletFoundation
+import Combine
 
 class RequestViewModelTests: XCTestCase {
+    private var cancelable = Set<AnyCancellable>()
 
     func testMyAddressText() {
         let account: Wallet = .make()
-        let viewModel = RequestViewModel(account: account)
+        let viewModel = RequestViewModel(account: account, domainResolutionService: FakeDomainResolutionService())
+        let expectation = self.expectation(description: "View model state has resolved")
+        let output = viewModel.transform(input: .init(copyEns: .empty(), copyAddress: .empty()))
 
-        XCTAssertEqual(account.address.eip55String, viewModel.myAddressText)
+        output.viewState
+            .sink { viewState in
+                XCTAssertEqual(account.address.eip55String, viewState.address, "matches address value")
+                //XCTAssertNotNil(viewState.qrCode, "has qr code")
+                if viewState.qrCode != nil {
+                    expectation.fulfill()
+                }
+            }.store(in: &cancelable)
+
+        wait(for: [expectation], timeout: 20)
     }
 }
