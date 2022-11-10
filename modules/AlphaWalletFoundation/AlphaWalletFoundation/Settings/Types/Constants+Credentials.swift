57e5f4f78d0bb28dc970c73469610cead3e430ff
Hwee-Boon Yar <hboon@motionobj.com>
2022-11-10 12:57:05 +0800

    [Refactor] Move credentials/APIs to single file

--- modules/AlphaWalletFoundation/AlphaWalletFoundation/Settings/Types/Constants+Credentials.swift
+++ modules/AlphaWalletFoundation/AlphaWalletFoundation/Settings/Types/Constants+Credentials.swift
@@ -32,6 +32,9 @@ extension Constants {
             }
         }
 
+        public static let analyticsKey = ""
+        public static let mailChimpListSpecificKey = ""
+        public static let walletConnectProjectId = "8ba9ee138960775e5231b70cc5ef1c3a"
         static let infuraKey = env("INFURAKEY") ?? "ad6d834b7a1e4d03a7fde92020616149"
         static let etherscanKey = env("ETHERSCANKEY") ?? "1PX7RG8H4HTDY8X55YRMCAKPZK476M23ZR"
         static let binanceSmartChainExplorerApiKey: String? = env("BINANCESMARTCHAINEXPLORERAPIKEY")
