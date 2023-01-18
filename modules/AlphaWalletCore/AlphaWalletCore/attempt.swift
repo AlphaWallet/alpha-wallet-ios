62f7eef0e7e109b1bb1dd96d24179a2c79118f35
Krypto Pank <krypto.pank@gmail.com>
2023-01-18 18:00:28 +0200

    tweak retry publisher and promise functions

--- modules/AlphaWalletCore/AlphaWalletCore/attempt.swift
+++ modules/AlphaWalletCore/AlphaWalletCore/attempt.swift
@@ -13,34 +13,16 @@ public func attempt<T>(maximumRetryCount: Int = 3, delayBeforeRetry: DispatchTim
     func attempt() -> Promise<T> {
         attempts += 1
         return body().recover { error -> Promise<T> in
-            guard let shouldOnlyRetryIf = shouldOnlyRetryIf, !shouldOnlyRetryIf(error) else { throw error }
+            //NOTE: can't use guard here!!!
+            if let shouldOnlyRetryIf = shouldOnlyRetryIf, !shouldOnlyRetryIf(error) { throw error }
             guard attempts < maximumRetryCount else { throw error }
 
             if case PMKError.cancelled = error {
                 throw error
             }
-            return after(delayBeforeRetry.nextRandomInterval(upTo: delayUpperRangeValueFrom0To)).then(on: nil, attempt)
+            return after(.seconds(Int.random(in: 0 ..< delayUpperRangeValueFrom0To))).then(on: nil, attempt)
         }
     }
 
     return attempt()
-}
-
-fileprivate extension DispatchTimeInterval {
-    func nextRandomInterval(upTo value: Int = 5) -> DispatchTimeInterval {
-        let jitter = Int.random(in: 0 ..< value)
-
-        switch self {
-        case .microseconds(let value):
-            return .microseconds(value + jitter)
-        case .milliseconds(let value):
-            return .milliseconds(value + jitter)
-        case .nanoseconds(let value):
-            return .nanoseconds(value + jitter)
-        case .never:
-            return .never
-        case .seconds(let value):
-            return .seconds(value + jitter)
-        }
-    }
 }
