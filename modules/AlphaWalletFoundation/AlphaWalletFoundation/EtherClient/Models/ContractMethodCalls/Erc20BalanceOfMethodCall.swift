c681224443b809af7df425f20c2fc91f10213859
Krypto Pank <krypto.pank@gmail.com>
2023-01-20 10:38:36 +0200

    Add typed contract method calls

---
+++ modules/AlphaWalletFoundation/AlphaWalletFoundation/EtherClient/Models/ContractMethodCalls/Erc20BalanceOfMethodCall.swift
@@ -0,0 +1,37 @@
+//
+//  Erc20BalanceOfMethodCall.swift
+//  AlphaWalletFoundation
+//
+//  Created by Vladyslav Shepitko on 17.01.2023.
+//
+
+import Foundation
+import AlphaWalletWeb3
+import BigInt
+
+struct Erc20BalanceOfMethodCall: ContractMethodCall {
+    typealias Response = BigInt
+
+    let contract: AlphaWallet.Address
+    let name: String = "balanceOf"
+    let abi: String = Web3.Utils.erc20ABI
+    var parameters: [AnyObject] { [address.eip55String] as [AnyObject] }
+
+    private let address: AlphaWallet.Address
+
+    init(contract: AlphaWallet.Address, address: AlphaWallet.Address) {
+        self.contract = contract
+        self.address = address
+    }
+
+    func response(from resultObject: Any) throws -> BigInt {
+        guard let dictionary = resultObject as? [String: AnyObject] else {
+            throw CastError(actualValue: resultObject, expectedType: BigInt.self)
+        }
+
+        guard let balanceOfUnknownType = dictionary["0"], let balance = BigInt(String(describing: balanceOfUnknownType)) else {
+            throw CastError(actualValue: dictionary["0"], expectedType: BigInt.self)
+        }
+        return balance
+    }
+}
---
+++ modules/AlphaWalletFoundation/AlphaWalletFoundation/EtherClient/Models/ContractMethodCalls/Erc20SupportsInterfaceMethodCall.swift
@@ -0,0 +1,37 @@
+//
+//  Erc20SupportsInterfaceMethodCall.swift
+//  AlphaWalletFoundation
+//
+//  Created by Vladyslav Shepitko on 17.01.2023.
+//
+
+import Foundation
+
+struct Erc20SupportsInterfaceMethodCall: ContractMethodCall {
+    typealias Response = Bool
+
+    private let function = GetInterfaceSupported165Encode()
+    private let hash: String
+
+    let contract: AlphaWallet.Address
+    var name: String { function.name }
+    var abi: String { function.abi }
+    var parameters: [AnyObject] { [hash] as [AnyObject] }
+
+    init(contract: AlphaWallet.Address, hash: String) {
+        self.contract = contract
+        self.hash = hash
+    }
+
+    func response(from resultObject: Any) throws -> Bool {
+        guard let dictionary = resultObject as? [String: AnyObject] else {
+            throw CastError(actualValue: resultObject, expectedType: [String: AnyObject].self)
+        }
+
+        guard let supported = dictionary["0"] as? Bool else {
+            throw CastError(actualValue: dictionary["0"], expectedType: Bool.self)
+        }
+
+        return supported
+    }
+}
