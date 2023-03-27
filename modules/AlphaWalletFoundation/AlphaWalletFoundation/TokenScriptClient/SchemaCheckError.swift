0baa7ead86acc4d1ced38b5a6baa485ee376dd7b
Krypto Pank <krypto.pank@gmail.com>
2023-03-27 15:47:23 +0300

    Remove unused type definitions

--- modules/AlphaWalletFoundation/AlphaWalletFoundation/TokenScriptClient/SchemaCheckError.swift
+++ modules/AlphaWalletFoundation/AlphaWalletFoundation/TokenScriptClient/SchemaCheckError.swift
@@ -7,13 +7,6 @@
 
 import Foundation
 
-public struct SchemaCheckError: LocalizedError {
-    var msg: String
-    public var errorDescription: String? {
-        return msg
-    }
-}
-
 public enum OpenURLError: Error {
     case unsupportedTokenScriptVersion
     case copyTokenScriptURL(_ url: URL, _ destinationURL: URL, error: Error)
