b1e6c2b39d40d9525b72dcbba40ae7cecaa21ad6
Krypto Pank <krypto.pank@gmail.com>
2023-05-18 16:21:43 +0300

    improve reloading token balance

--- modules/AlphaWalletFoundation/AlphaWalletFoundation/WalletBalance/WalletBalanceFetcher.swift
+++ modules/AlphaWalletFoundation/AlphaWalletFoundation/WalletBalance/WalletBalanceFetcher.swift
@@ -8,6 +8,7 @@
 import Foundation
 import BigInt
 import Combine
+import AlphaWalletCore
 
 public protocol WalletBalanceFetcherType: AnyObject {
     var walletBalance: AnyPublisher<WalletBalance, Never> { get }
@@ -18,15 +19,15 @@ public protocol WalletBalanceFetcherType: AnyObject {
 
 public class WalletBalanceFetcher: NSObject, WalletBalanceFetcherType {
     private static let updateBalanceInterval: TimeInterval = 60
-    private var timer: Timer?
+    private let scheduler: Scheduler
     private let wallet: Wallet
     private var cancelable = Set<AnyCancellable>()
     private let tokensPipeline: TokensProcessingPipeline
     private let tokensService: TokensService
-    private lazy var walletBalanceSubject = CurrentValueSubject<WalletBalance, Never>(WalletBalance(wallet: wallet, tokens: [], currency: currencyService.currency))
+    private lazy var subject = CurrentValueSubject<WalletBalance, Never>(WalletBalance(wallet: wallet, tokens: [], currency: currencyService.currency))
     private let currencyService: CurrencyService
     public var walletBalance: AnyPublisher<WalletBalance, Never> {
-        walletBalanceSubject.eraseToAnyPublisher()
+        subject.eraseToAnyPublisher()
     }
 
     public init(wallet: Wallet,
@@ -38,20 +39,35 @@ public class WalletBalanceFetcher: NSObject, WalletBalanceFetcherType {
         self.wallet = wallet
         self.tokensPipeline = tokensPipeline
         self.currencyService = currencyService
+        let provider = ReloadTokensSchedulerProvider(tokensService: tokensService)
+        self.scheduler = Scheduler(provider: provider)
         super.init()
     }
 
     public func start() {
         guard !isRunningTests() else { return }
 
-        timer = Timer.scheduledTimer(withTimeInterval: Self.updateBalanceInterval, repeats: true) { [weak self] _ in
-            self?.refreshBalance(updatePolicy: .all)
-        }
+        scheduler.start()
 
         tokensPipeline.tokenViewModels
             .map { [wallet, currencyService] in WalletBalance(wallet: wallet, tokens: $0, currency: currencyService.currency) }
             .removeDuplicates()
-            .assign(to: \.value, on: walletBalanceSubject)
+            .assign(to: \.value, on: subject)
+            .store(in: &cancelable)
+
+        NotificationCenter.default.applicationState
+            .receive(on: RunLoop.main)
+            .sink { [weak scheduler] state in
+                switch state {
+                case .didEnterBackground:
+                    scheduler?.cancel()
+                case .willEnterForeground:
+                    scheduler?.restart()
+                }
+            }.store(in: &cancelable)
+
+        tokensService.providersHasChanged
+            .sink { [weak scheduler] _ in scheduler?.restart(force: true) }
             .store(in: &cancelable)
     }
 
@@ -64,7 +80,26 @@ public class WalletBalanceFetcher: NSObject, WalletBalanceFetcherType {
     }
 
     public func stop() {
-        timer?.invalidate()
-        timer = nil
+        scheduler.cancel()
+    }
+
+    private class ReloadTokensSchedulerProvider: SchedulerProvider {
+        private let tokensService: TokensService
+
+        let name: String = ""
+        let interval: TimeInterval = 60
+
+        var operation: AnyPublisher<Void, PromiseError> {
+            AnyPublisher.create { [tokensService] seal in
+                seal.send(completion: .finished)
+                tokensService.refreshBalance(updatePolicy: .all)
+
+                return AnyCancellable { }
+            }
+        }
+
+        init(tokensService: TokensService) {
+            self.tokensService = tokensService
+        }
     }
 }
