ca5ec023bc407a04329d4db891e1556b95b45e93
Hwee-Boon Yar <hboon@motionobj.com>
2023-07-04 12:03:43 +0800

    [Refactoring] Extract/move type and files to appropriate place

--- AlphaWallet/WalletConnect/Coordinator/AcceptProposalCoordinatorBridgeToPromise.swift
+++ AlphaWallet/WalletConnect/Coordinator/AcceptProposalCoordinatorBridgeToPromise.swift
@@ -9,7 +9,6 @@ import PromiseKit
 import AlphaWalletFoundation
 
 private class AcceptProposalCoordinatorBridgeToPromise {
-
     private let (promiseToReturn, seal) = Promise<ProposalResult>.pending()
     private var retainCycle: AcceptProposalCoordinatorBridgeToPromise?
 
@@ -44,46 +43,5 @@ extension AcceptProposalCoordinatorBridgeToPromise: AcceptProposalCoordinatorDel
 extension AcceptProposalCoordinator {
     static func promise(_ navigationController: UINavigationController, coordinator: Coordinator, proposalType: ProposalType, analytics: AnalyticsLogger, restartHandler: RestartQueueHandler) -> Promise<ProposalResult> {
         return AcceptProposalCoordinatorBridgeToPromise(navigationController: navigationController, coordinator: coordinator, proposalType: proposalType, analytics: analytics, restartHandler: restartHandler).promise
-    }
-}
-
-private class AcceptAuthRequestCoordinatorBridgeToPromise {
-    private let (promiseToReturn, seal) = Promise<AuthRequestResult>.pending()
-    private var retainCycle: AcceptAuthRequestCoordinatorBridgeToPromise?
-
-    init(navigationController: UINavigationController,
-         coordinator: Coordinator,
-         authRequest: AlphaWallet.WalletConnect.AuthRequest,
-         analytics: AnalyticsLogger) {
-
-        retainCycle = self
-
-        let newCoordinator = AcceptAuthRequestCoordinator(analytics: analytics, authRequest: authRequest, navigationController: navigationController)
-        newCoordinator.delegate = self
-        coordinator.addCoordinator(newCoordinator)
-
-        promiseToReturn.ensure {
-            // ensure we break the retain cycle
-            self.retainCycle = nil
-            coordinator.removeCoordinator(newCoordinator)
-        }.cauterize()
-
-        newCoordinator.start()
-    }
-
-    var promise: Promise<AuthRequestResult> {
-        return promiseToReturn
-    }
-}
-
-extension AcceptAuthRequestCoordinatorBridgeToPromise: AcceptAuthRequestCoordinatorDelegate {
-    func coordinator(_ coordinator: AcceptAuthRequestCoordinator, didComplete result: AuthRequestResult) {
-        seal.fulfill(result)
-    }
-}
-
-extension AcceptAuthRequestCoordinator {
-    static func promise(_ navigationController: UINavigationController, coordinator: Coordinator, authRequest: AlphaWallet.WalletConnect.AuthRequest, analytics: AnalyticsLogger) -> Promise<AuthRequestResult> {
-        return AcceptAuthRequestCoordinatorBridgeToPromise(navigationController: navigationController, coordinator: coordinator, authRequest: authRequest, analytics: analytics).promise
     }
 }
