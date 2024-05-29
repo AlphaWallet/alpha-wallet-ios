395c8202c616b5b3fa097ce6f7aecc60bdd80273
Hwee-Boon Yar <hboon@motionobj.com>
2024-05-29 12:19:28 +0800

    Switch from NSLog to os.log for AlphaWalletAttestation

--- modules/AlphaWalletAttestation/AlphaWalletAttestation/Logger.swift
+++ modules/AlphaWalletAttestation/AlphaWalletAttestation/Logger.swift
@@ -1,13 +1,16 @@
 // Copyright © 2023 Stormbird PTE. LTD.
 
 import Foundation
+import os.log
+
+private let logger = Logger(subsystem: Bundle.main.bundleIdentifier!, category: "attestation")
 
 func infoLog(_ message: Any, callerFunctionName: String = #function) {
     guard Attestation.isLoggingEnabled else { return }
-    NSLog("\(message) from: \(callerFunctionName)")
+    logger.info("\(String(describing: message)) from: \(callerFunctionName)")
 }
 
 func errorLog(_ message: Any, callerFunctionName: String = #function) {
     guard Attestation.isLoggingEnabled else { return }
-    NSLog("\(message) from: \(callerFunctionName)")
+    logger.error("\(String(describing: message)) from: \(callerFunctionName)")
 }
