81ea2e7c5767225a76820a7a56f267e524443e4a
Hwee-Boon Yar <hboon@motionobj.com>
2024-05-29 12:21:24 +0800

    Switch from NSLog to os.log for AlphaWalletTrackAPICalls

--- modules/AlphaWalletTrackAPICalls/AlphaWalletTrackAPICalls/Logger.swift
+++ modules/AlphaWalletTrackAPICalls/AlphaWalletTrackAPICalls/Logger.swift
@@ -1,7 +1,10 @@
 // Copyright © 2022 Stormbird PTE. LTD.
 
 import Foundation
+import os.log
+
+private let logger = Logger(subsystem: Bundle.main.bundleIdentifier!, category: "trackAPICalls")
 
 func infoLog(_ message: Any, callerFunctionName: String = #function) {
-    NSLog("\(message) from: \(callerFunctionName)")
+    logger.info("\(String(describing: message)) from: \(callerFunctionName)")
 }
