769a0109716beb32c272fcf3daf48191eb87d5ab
anon-GM <andystorrs@hotmail.com>
2026-07-05 21:04:44 +0100

    Add GridMind dapp browser entry

--- AlphaWallet/Browser/ViewModel/Dapps.swift
+++ AlphaWallet/Browser/ViewModel/Dapps.swift
@@ -7,6 +7,7 @@ This comment is here with the wrong syntax (no leading //) to force a build erro
 // swiftlint:enable no_space_in_method_call
 enum OriginalDapps {
     static let masterList = [
+        Dapp(name: "GridMind", description: "Testnet wallet and explorer for GridMind asset flows", url: "https://www.grid-mind.com/blockchain", cat: "Tool"),
         Dapp(name: "COINWAR", description: "DopeWars Fork for Crypto", url: "https://coinwar.app", cat: "Games"),
         Dapp(name: "Rare Coin", description: "Free Crypto Faucet & Yeild Farming", url: "make.rare.claims", cat: "Tools"),
         Dapp(name: "Eporio", description: "The cheaper marketplace for NFT - Non Fungible Tokens", url: "https://epor.io", cat: "Marketplace"),
